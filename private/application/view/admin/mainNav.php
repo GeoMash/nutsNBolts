ca37bb7a2f47aa14a885230a5159da7a74eb2756
unknown <tim_000@.(none)>
2013-05-11 01:43:48 +0800

    Navigation work.

---
+++ private/application/view/admin/mainNav.php
@@ -0,0 +1,80 @@
+<ul class="nav nav-collapse collapse nav-collapse-primary">
+	<li class="active">
+		<span class="glow"></span>
+		<a href="dashboard.html">
+			<i class="icon-dashboard icon-2x"></i>
+			<span>Dashboard</span>
+		</a>
+	</li>
+	<li class="dark-nav">
+		<span class="glow"></span>
+		<a class="accordion-toggle collapsed" data-toggle="collapse" href="#content-subs">
+			<i class="icon-edit icon-2x"></i>
+			<span>
+				Content
+				<i class="icon-caret-down"></i>
+			</span>
+		</a>
+		<ul id="content-subs" class="collapse ">
+			<li class="">
+				<a href="/admin/content/create/1">
+					<i class="icon-list"></i> News Article
+				</a>
+			</li>
+			<li class="">
+				<a href="/admin/content/create/2">
+					<i class="icon-file"></i> Blog Entry
+				</a>
+			</li>
+			<li class="">
+				<a href="/admin/content/create/3">
+					<i class="icon-picture"></i> Gallery
+				</a>
+			</li>
+		</ul>
+	</li>
+	<li class="dark-nav">
+		<span class="glow"></span>
+		<a data-toggle="collapse" href="#content-config-subs">
+			<i class="icon-cogs icon-2x"></i>
+			<span>
+				Configure Content
+				<i class="icon-caret-down"></i>
+			</span>
+		</a>
+		<ul id="content-config-subs" class="collapse">
+			<li class="">
+				<a href="/admin/configurecontent/types">
+					<i class="icon-th"></i> Types
+				</a>
+			</li>
+			<li class="">
+				<a href="/admin/configurecontent/widgets">
+					<i class="icon-beaker"></i> Widgets
+				</a>
+			</li>
+		</ul>
+	</li>
+	<li class="dark-nav">
+		<span class="glow"></span>
+		<a data-toggle="collapse" href="#settings-subs">
+			<i class="icon-wrench icon-2x"></i>
+			<span>
+				System Settings
+				<i class="icon-caret-down"></i>
+			</span>
+		</a>
+		<ul id="settings-subs" class="collapse">
+			<li class="">
+				<a href="/admin/settings/general">
+					<i class="icon-circle-blank"></i> General
+				</a>
+			</li>
+			<li class="">
+				<a href="/admin/settings/users">
+					<i class="icon-user"></i> Users
+				</a>
+			</li>
+		</ul>
+	</li>
+</ul>
---
+++ private/application/view/admin/topNav.php
@@ -0,0 +1,53 @@
+<div class="nav-collapse nav-collapse-top collapse">
+	<ul class="nav full pull-right">
+		<li class="dropdown user-avatar">
+
+			<!-- the dropdown has a custom user-avatar class, this is the small avatar with the badge -->
+
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				<span>
+					<img class="menu-avatar" src="/images/avatars/avatar1.jpg" /> <span>John Smith <i class="icon-caret-down"></i></span>
+					<span class="badge badge-dark-red">5</span>
+				</span>
+			</a>
+
+			<ul class="dropdown-menu">
+
+				<!-- the first element is the one with the big avatar, add a with-image class to it -->
+
+				<li class="with-image">
+					<div class="avatar">
+						<img src="/images/avatars/avatar1.jpg" />
+					</div>
+					<span>John Smith</span>
+				</li>
+
+				<li class="divider"></li>
+
+				<li><a href="#"><i class="icon-user"></i> <span>Profile</span></a></li>
+				<li><a href="#"><i class="icon-cog"></i> <span>Settings</span></a></li>
+				<li><a href="#"><i class="icon-envelope"></i> <span>Messages</span> <span class="label label-dark-red pull-right">5</span></a></li>
+				<li><a href="#"><i class="icon-off"></i> <span>Logout</span></a></li>
+			</ul>
+		</li>
+	</ul>
+
+	<form class="navbar-search pull-right">
+		<input type="text" class="search-query animated" placeholder="Search">
+		<i class="icon-search"></i>
+	</form>
+
+	<ul class="nav pull-right">
+		<li class="active"><a href="#" title="Go home"><i class="icon-home"></i> Home</a></li>
+		<li><a href="#" title="Manage users"><i class="icon-user"></i> Users</a></li>
+		<li class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+			<ul class="dropdown-menu">
+				<li><a href="#">Action</a></li>
+				<li><a href="#">Another action</a></li>
+				<li><a href="#">Some link</a></li>
+				<li><a href="#">One more separated link</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
