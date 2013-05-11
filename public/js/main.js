071152b4353a2896dba3f5f513f93754fe4b31a0
tim@praxisbt.com <tim@praxisbt.com>
2013-05-11 15:23:05 +0800

    Properly Implemenrted Require JS with jquery.

---
+++ public/js/main.js
@@ -0,0 +1,58 @@
+requirejs.config
+(
+	{
+		waitSeconds:	3,
+		baseUrl:		'/js/vendor/',
+		paths:
+		{
+			jskk:				'jskk/jskk-1.1.0.min',
+			'jskk-optional':	'jskk/jskk-1.1.0-optional.min',
+			$JSKK:				'jskk',
+			extension:			'extension',
+			plupload:			'plupload',
+			nutsnbolts:			'../nutsnbolts'
+			
+		}
+	}
+);
+
+requirejs
+(
+	[
+		'jquery',
+		'jquery/ui',
+		'jquery/browser',
+		'jquery/gritter',
+		'jquery/icheck',
+		'jquery/uniform',
+		'jquery/validationEngine',
+		'jquery/validationEngine-en',
+		'jquery/ibutton',
+		'jquery/select2',
+		'bootstrap/bootstrap',
+		'jskk',
+		'plupload/plupload.full'
+	],
+	function()
+	{
+		requirejs
+		(
+			[
+				'jskk-optional',
+				'extension/function',
+				'extension/string'
+			],
+			function()
+			{
+				$JSKK.require
+				(
+					'nutsnbolts.Application',
+					function()
+					{
+						$application=new nutsnbolts.Application();
+					}
+				);
+			}
+		);
+	}
+);
