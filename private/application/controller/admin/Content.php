81a2adf8e49770062d0d20f7b2d1b643f82c6619
tim@praxisbt.com <tim@praxisbt.com>
2013-05-19 01:03:01 +0800

    Content management in its full :)

--- private/application/controller/admin/Content.php
+++ private/application/controller/admin/Content.php
@@ -7,14 +7,15 @@ namespace application\controller\admin
 	{
 		private $typeID=null;
 		
-		public function view($id)
+		public function view($typeID)
 		{
-			$this->typeID=$id;
-			$contentType=$this->model->ContentType->read($id);
+			$this->typeID=$typeID;
+			$contentType=$this->model->ContentType->read($this->typeID);
 			
 			$this->addBreadcrumb('Content','icon-edit');
 			$this->addBreadcrumb($contentType[0]['name'],$contentType[0]['icon']);
 			$this->setContentView('admin/content/view');
+			$this->view->setVar('contentTypeId',$this->typeID);
 			$this->view->setVar('tableHeaderText',$contentType[0]['name']);
 			
 			$this->view->getContext()
@@ -30,9 +31,101 @@ namespace application\controller\admin
 			$this->view->render();
 		}
 		
-		public function edit()
+		public function add($typeID)
 		{
+			if (!$this->request->get('title'))
+			{
+				$this->generateContentParts($typeID);
+				$this->setContentView('admin/content/addEdit');
+				$this->addBreadcrumb('Add Content','icon-pencil');
+				$this->view->render();
+			}
+			else
+			{
+				$record=$this->request->getAll();
+				$record['content_type_id']=$typeID;
+				//TODO last_user_id
+				$id=$this->model->Node->handleRecord($record);
+				$this->redirect('/admin/content/edit/'.$id);
+			}
+		}
+		
+		public function edit($id)
+		{
+			if ($this->request->get('id'))
+			{
+				$this->model->Node->handleRecord($this->request->getAll());
+			}
+			$node		=$this->model->Node->read(array('id'=>$id));
+			$nodeParts	=$this->model->NodePart->read(array('node_id'=>$id));
+			$contentType=$this->model->ContentType->readWithParts($node[0]['content_type_id']);
+			$parts		=array();
+			
+			for ($i=0,$j=count($contentType); $i<$j; $i++)
+			{
+				$thisNodePart=null;
+				for ($k=0,$l=count($nodeParts); $k<$l; $k++)
+				{
+					if ($nodeParts[$k]['content_part_id']==$contentType[$i]['content_part_id'])
+					{
+						$thisNodePart=$nodeParts[$k];
+					}
+				}
+				$formElId='node_part_id_'.$contentType[$i]['content_part_id'].'_';
+				$formElId.=($thisNodePart)?$thisNodePart['id']:'0';
+				$input=str_replace
+				(
+					array('{name}','{value}'),
+					array($formElId,$thisNodePart?$thisNodePart['value']:''),
+					$contentType[$i]['template']
+				);
+				$parts[]=<<<HTML
+<div class="control-group">
+	<label class="control-label">{$contentType[$i]['label']}</label>
+	<div class="controls">
+		{$input}
+	</div>
+</div>
+HTML;
+			}
+			
+			$this->view->setVars($node[0]);
+			$this->view->setVar('contentType',$contentType[0]['name']);
+			$this->view->setVar('contentTypeIcon',$contentType[0]['icon']);
+			$this->view->setVar('parts',implode('',$parts));
+			
+			
+			$this->setContentView('admin/content/addEdit');
+			$this->addBreadcrumb('Edit Content','icon-pencil');
 			$this->view->render();
+		}
+		
+		private function generateContentParts($contentTypeId=null)
+		{
+			$contentType=$this->model->ContentType->readWithParts($contentTypeId);
+			$parts		=array();
+			
+			for ($i=0,$j=count($contentType); $i<$j; $i++)
+			{
+				$formElId='node_part_id_'.$contentType[$i]['content_part_id'].'_0';
+				$input=str_replace
+				(
+					array('{name}','{value}'),
+					array($formElId,''),
+					$contentType[$i]['template']
+				);
+				$parts[]=<<<HTML
+<div class="control-group">
+	<label class="control-label">{$contentType[$i]['label']}</label>
+	<div class="controls">
+		{$input}
+	</div>
+</div>
+HTML;
+			}
+			$this->view->setVar('contentType',$contentType[0]['name']);
+			$this->view->setVar('contentTypeIcon',$contentType[0]['icon']);
+			$this->view->setVar('parts',implode('',$parts));
 		}
 		
 		public function generateContentRows()
