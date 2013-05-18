197de096765507b73bde129394aca6046e58bf2d
tim@praxisbt.com <tim@praxisbt.com>
2013-05-19 02:38:07 +0800

    Convenience function for getting normalized node content.

--- private/application/model/NodeMap.php
+++ private/application/model/NodeMap.php
@@ -5,7 +5,38 @@ namespace application\model
 	
 	class NodeMap extends NodeMapBase	
 	{
-		
+		public function getNodesForPath($path)
+		{
+			$query=<<<SQL
+			SELECT node.*
+			FROM node
+			LEFT JOIN node_map ON node_map.node_id=node.id
+			WHERE node_map.url=?;
+SQL;
+			if ($this->db->select($query,array($path)))
+			{
+				$records=$this->db->result('assoc');
+				for ($i=0,$j=count($records); $i<$j; $i++)
+				{
+					$query=<<<SQL
+					SELECT node_part.value,content_part.ref
+					FROM node_part
+					LEFT JOIN content_part ON content_part.id=node_part.content_part_id
+					WHERE node_part.node_id=?;
+SQL;
+					if ($this->db->select($query,array($records[$i]['id'])))
+					{
+						$nodePart=$this->db->result('assoc');
+						for ($k=0,$l=count($nodePart); $k<$l; $k++)
+						{
+							$records[$i][$nodePart[$k]['ref']]=$nodePart[$k]['value'];
+						}
+					}
+				}
+				return isset($records)?$records:false;
+			}
+			return false;
+		}
 	}
 }
 ?>
