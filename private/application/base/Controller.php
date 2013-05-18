60b8cfc2f8fec49545a6fbe6429c30ade44ce5b6
tim@praxisbt.com <tim@praxisbt.com>
2013-05-19 01:03:46 +0800

    New base base controller :p

---
+++ private/application/base/Controller.php
@@ -0,0 +1,21 @@
+<?php
+namespace application\base
+{
+	use nutshell\plugin\mvc\Mvc;
+	use nutshell\plugin\mvc\Controller as MvcController;
+	
+	class Controller extends MvcController
+	{
+		// public function __construct(Mvc $MVC)
+		// {
+			
+		// }
+		
+		// public function route()
+		// {
+			
+		// }
+		// 
+	}
+}
+?>
