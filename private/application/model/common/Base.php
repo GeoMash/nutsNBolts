550dcb9d4e48b59b9f2a208d615feffc55df4837
tim@praxisbt.com <tim@praxisbt.com>
2013-05-21 00:41:09 +0800

    Added a base generic handleRecord method.

--- private/application/model/common/Base.php
+++ private/application/model/common/Base.php
@@ -5,6 +5,20 @@ namespace application\model\common
 
 	abstract class Base extends CRUD
 	{
-		
+		public function handleRecord($record)
+		{
+			if (!isset($record['status']))$record['status']=0;
+			//For Updates
+			if (isset($record['id']) && is_numeric($record['id']))
+			{
+				$this->update($record,array('id'=>$record['id']));
+			}
+			//For Inserts
+			else
+			{
+				return $this->insertAssoc($record);
+			}
+			return false;
+		}
 	}
 }
