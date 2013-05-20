42500c58aeccb16f618e1c25446cd474456fb7f4
tim@praxisbt.com <tim@praxisbt.com>
2013-05-21 00:41:36 +0800

    Page management support for configure pages.

--- private/application/controller/admin/ConfigurePages.php
+++ private/application/controller/admin/ConfigurePages.php
@@ -16,21 +16,9 @@ namespace application\controller\admin
 			$this->addBreadcrumb('Types','icon-th-large');
 			switch ($action)
 			{
-				case 'add':
-				{
-					$this->addType();
-					break;
-				}
-				case 'edit':
-				{
-					$this->editType($id);
-					break;
-				}
-				case 'remove':
-				{
-					$this->removeType($id);
-					break;
-				}
+				case 'add':		$this->addType();		break;
+				case 'edit':	$this->editType($id);	break;
+				case 'remove':	$this->removeType($id);	break;
 				default:
 				{
 					$this->setContentView('admin/configurePages/types');
@@ -48,17 +36,66 @@ namespace application\controller\admin
 			$this->view->render();
 		}
 		
+		public function pages($action=null,$id=null)
+		{
+			$this->addBreadcrumb('Configure Pages','icon-cogs');
+			$this->addBreadcrumb('Pages','icon-copy');
+			switch ($action)
+			{
+				case 'add':		$this->addPage();		break;
+				case 'edit':	$this->editPage($id);	break;
+				case 'remove':	$this->removePage($id);	break;
+				default:
+				{
+					$this->setContentView('admin/configurePages/pages');
+					$this->view->getContext()
+					->registerCallback
+					(	
+						'getPagesList',
+						function()
+						{
+							print $this->generatePageList();
+						}
+					);
+				}
+			}
+			$this->view->getContext()
+			->registerCallback
+			(	
+				'getPageTypeOptions',
+				function($options=null)
+				{
+					print $this->getPageTypeOptions($options);
+				}
+			);
+			$this->view->render();
+		}
+		
 		private function addType()
 		{
 			if (!$this->request->get('name'))
 			{
-				$this->addBreadcrumb('Add Page Type','icon-plus');
+				$this->addBreadcrumb('Add Page','icon-copy');
 				$this->setContentView('admin/configurePages/addEditType');
 			}
 			else
 			{
 				$id=$this->model->PageType->handleRecord($this->request->getAll());
 				$this->redirect('/admin/configurepages/types/edit/'.$id);
+			}
+		}
+		
+		private function addPage()
+		{
+			if (!$this->request->get('name'))
+			{
+				$this->addBreadcrumb('Add Page','icon-plus');
+				$this->setContentView('admin/configurePages/addEditPage');
+			}
+			else
+			{
+				$id=$this->model->Page->handleRecord($this->request->getAll());
+				$this->redirect('/admin/configurepages/pages/edit/'.$id);
 			}
 		}
 		
@@ -80,10 +117,34 @@ namespace application\controller\admin
 			}
 		}
 		
+		private function editPage($id)
+		{
+			if ($this->request->get('id'))
+			{
+				$this->model->Page->handleRecord($this->request->getAll());
+			}
+			$this->addBreadcrumb('Edit Page','icon-edit');
+			$this->setContentView('admin/configurePages/addEditPage');
+			if ($record=$this->model->Page->read($id))
+			{
+				$this->view->setVars($record[0]);
+			}
+			else
+			{
+				$this->view->setVar('record',array());
+			}
+		}
+		
 		public function removeType($id)
 		{
-			$this->model->ContentType->handleDeleteRecord($id);
-			$this->redirect('/admin/configurecontent/types/');
+			$this->model->PageType->handleDeleteRecord($id);
+			$this->redirect('/admin/configurepages/types/');
+		}
+		
+		public function removePage($id)
+		{
+			$this->model->Page->handleDeleteRecord($id);
+			$this->redirect('/admin/configurepages/pages/');
 		}
 		
 		public function generateTypeList()
@@ -106,6 +167,40 @@ namespace application\controller\admin
 HTML;
 			}
 			return implode('',$html);
+		}
+		
+		public function generatePageList()
+		{
+			$html=array();
+			$pages=$this->model->Page->read();
+			for ($i=0,$j=count($pages); $i<$j; $i++)
+			{
+				$html[]=<<<HTML
+<div class="box-section news with-icons relative">
+	<div class="avatar blue"></div>
+	<a href="/admin/configurepages/pages/remove/{$pages[$i]['id']}">
+		<span class="triangle-button red"><i class="icon-remove"></i></span>
+	</a>
+	<div class="news-content">
+		<div class="news-title"><a href="/admin/configurepages/pages/edit/{$pages[$i]['id']}">{$pages[$i]['title']}</a></div>
+		<div class="news-text">{$pages[$i]['description']}</div>
+	</div>
+</div>
+HTML;
+			}
+			return implode('',$html);
+		}
+		
+		public function getPageTypeOptions($selectedId=null)
+		{
+			$return=array();
+			$pageTypes=$this->model->PageType->read(array('status'=>1));
+			for ($i=0,$j=count($pageTypes); $i<$j; $i++)
+			{
+				$selected=($pageTypes[$i]['id']==$selectedId)?'selected':'';
+				$return[]='<option value="'.$pageTypes[$i]['id'].'" '.$selected.'>'.$pageTypes[$i]['name'].'</option>';
+			}
+			return implode('',$return);
 		}
 	}
 }
