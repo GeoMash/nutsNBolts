e097aa9ea805f9cff5a0790883c886665b10084d
tim@praxisbt.com <tim@praxisbt.com>
2013-05-21 00:41:21 +0800

    Removed handleRecord method. Its now based.

--- private/application/model/PageType.php
+++ private/application/model/PageType.php
@@ -5,21 +5,7 @@ namespace application\model
 	
 	class PageType extends PageTypeBase	
 	{
-		public function handleRecord($record)
-		{
-			if (!isset($record['status']))$record['status']=0;
-			//For Updates
-			if (isset($record['id']) && is_numeric($record['id']))
-			{
-				$this->update($record,array('id'=>$record['id']));
-			}
-			//For Inserts
-			else
-			{
-				return $this->insertAssoc($record);
-			}
-			return false;
-		}
+		
 	}
 }
 ?>
