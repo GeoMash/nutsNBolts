750df790a5f19ed5bdb4bfff2ba463a66acc7a8e
Timothy Chandler <tim@praxisbt.com>
2013-08-06 06:26:03 +0800

    Template fixes.

--- widget/select/assets/widget.tpl.php
+++ widget/select/assets/widget.tpl.php
@@ -1,9 +1,9 @@
 <select name="<?php $tpl->name; ?>">
 	<?php
-	$options=$tpl->get('config')->options;
+	$options=$tpl->get('options');
 	for ($i=0,$j=count($options); $i<$j; $i++)
 	{
-		$selectd=($options[$i]->value!=$tpl->get('value'))?'':'selected';
+		$selected=($options[$i]->value!=$tpl->get('value'))?'':'selected';
 		print '<option value="'.$options[$i]->value.'" '.$selected.'>'.$options[$i]->label.'</option>';
 	}
 	?>
