a96c6f126d9c4eae1de6ea5889190f4741fce772
Timothy Chandler <tim@praxisbt.com>
2013-08-11 20:51:07 +0800

    Implemented page map.

--- controller/Index.php
+++ controller/Index.php
@@ -25,13 +25,11 @@ namespace application\nutsnbolts\controller
 			$binding		=$this->application->nutsnbolts->getSiteBinding($this->getSiteRef());
 			$applicationName=strtolower(ObjectHelper::getBaseClassName(get_class($binding['application'])));
 			$path			=$this->getPath();
-			$page			=$this->model->Page->read(array('url'=>$path));
-
-			if (count($page))
+			$page			=$this->model->PageMap->getPageFromPath($path);
+			if ($page)
 			{
-				$this->page		=$page[0];
+				$this->page		=$page;
 				$this->pageType	=$this->model->PageType->read(array('id'=>$this->page['page_type_id']))[0];
-
 				$this->nodes	=$this->model->NodeMap->getNodesForPath($path);
 				$this->viewPath	='..'._DS_.'..'._DS_.$applicationName._DS_.'view'._DS_;
 				
