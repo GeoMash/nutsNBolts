f0777095aef039568d5c50af70c962af89db8c95
unknown <tim_000@.(none)>
2013-08-13 03:26:49 +0800

    Fixed another app reference.

--- base/Controller.php
+++ base/Controller.php
@@ -22,7 +22,7 @@ namespace application\nutsNBolts\base
 			{
 				die('No site registered for this domain!');
 			}
-			if (!$this->application->nutsnbolts->getSiteBinding($this->getSiteRef()))
+			if (!$this->application->nutsNBolts->getSiteBinding($this->getSiteRef()))
 			{
 				die('No site bound for this domain!');
 			}
