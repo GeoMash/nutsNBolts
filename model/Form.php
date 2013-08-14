23bd100a04121f69a40476501c76ba6c6bf4c3b2
Timothy Chandler <tim@praxisbt.com>
2013-08-14 19:01:07 +0800

    New form models.

---
+++ model/Form.php
@@ -0,0 +1,11 @@
+<?php
+namespace application\nutsNBolts\model
+{
+	use application\nutsNBolts\model\base\Form as FormBase;
+	
+	class Form extends FormBase	
+	{
+		
+	}
+}
+?>
---
+++ model/FormSubmission.php
@@ -0,0 +1,11 @@
+<?php
+namespace application\nutsNBolts\model
+{
+	use application\nutsNBolts\model\base\FormSubmission as FormSubmissionBase;
+	
+	class FormSubmission extends FormSubmissionBase	
+	{
+		
+	}
+}
+?>
