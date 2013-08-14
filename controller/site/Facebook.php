e159b8b8c86b99bfe35f449f691bd253969ac719
Ego <ego@praxisbt.com>
2013-08-15 04:08:16 +0800

    facebook plugin is done, encoding the data is the last part left

--- controller/site/Facebook.php
+++ controller/site/Facebook.php
@@ -14,9 +14,20 @@ namespace application\nutsNBolts\controller\site
 		
 		public function save()
 		{
-			$this->request->get('email');
-			//store in model.
+			$params=array();
+			 		// foreach($user as $key)
+			 		// {
+			 			// $params['fb_uid']=$this->request->get('fb_uid');
+			 			// $params['first_name']=$this->request->get('firstName');
+			 			// $params['last_name']=$this->request->get('lastName');
+			 			// $params['email']=$this->request->get('email');
+			 			// $params['gender']=$this->request->get('gender');
+			 			
+			 		$result=$this->model->Facebook->insertAssoc($this->request->getAll());
+			 		var_dump($result);
+			 //store in model.
 		}
+		
 	}
 }
 ?>
---
+++ model/Facebook.php
@@ -0,0 +1,11 @@
+<?php
+namespace application\nutsNBolts\model
+{
+	use application\nutsNBolts\model\base\Facebook as FacebookBase;
+	
+	class Facebook extends FacebookBase	
+	{
+		
+	}
+}
+?>
