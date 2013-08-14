bfa2049ea9fe2ca6fe7ad30fb8223f432151a1ed
Timothy Chandler <tim@praxisbt.com>
2013-08-15 03:52:28 +0800

    Removed avatar and profile link.

--- view/admin/topNav.php
+++ view/admin/topNav.php
@@ -9,7 +9,8 @@ $user=$tpl->get('user');
 
 			<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 				<span>
-					<img class="menu-avatar" src="/images/avatars/avatar1.jpg" /> <span><?php print $user['name_first'].' '.$user['name_last']; ?> <i class="icon-caret-down"></i></span>
+					<!-- <img class="menu-avatar" src="/images/avatars/avatar1.jpg" />  -->
+					<span><?php print $user['name_first'].' '.$user['name_last']; ?> <i class="icon-caret-down"></i></span>
 				</span>
 			</a>
 
@@ -19,14 +20,14 @@ $user=$tpl->get('user');
 
 				<li class="with-image">
 					<div class="avatar">
-						<img src="/images/avatars/avatar1.jpg" />
+						<!-- <img src="/images/avatars/avatar1.jpg" /> -->
 					</div>
 					<span><?php print $user['name_first'].' '.$user['name_last']; ?></span>
 				</li>
 
 				<li class="divider"></li>
 
-				<li><a href="#"><i class="icon-user"></i> <span>Profile</span></a></li>
+				<!-- <li><a href="#"><i class="icon-user"></i> <span>Profile</span></a></li> -->
 				<li><a href="/admin/logout"><i class="icon-off"></i> <span>Logout</span></a></li>
 			</ul>
 		</li>
