8b5bc1a3dae14a9dd7219c47f4933d2075527af5
Timothy Chandler <tim@praxisbt.com>
2013-08-14 12:23:18 +0800

    Completed Image Selector.

--- widget/image/assets/Main.js
+++ widget/image/assets/Main.js
@@ -8,13 +8,37 @@ $JSKK.Class.create
 (
 	{},
 	{
-		init: function()
+		widget:	null,
+		init: function(id)
 		{
-			console.debug('HELLO IMAGE MAIN');
+			this.widget=$('[data-id="'+id+'"]')
+			$application.registerAction('widget.image.main.browseImage',this.browseImage.bind(this))
 		},
 		browseImage: function()
 		{
-			
+			$application.fileManager.show
+			(
+				{
+					buttons:
+					[
+						{
+							label:		'Select Image',
+							icon:		'icon-ok',
+							handler:	this.onSelect.bind(this)
+						}
+					]
+				}
+			);
+		},
+		onSelect: function(fileManager)
+		{
+			fileManager.hide();
+			var selected=fileManager.getSelected();
+			this.widget.find('.imageSelector').addClass('selected');
+			this.widget.find('input').val(selected.src);
+			this.widget.find('.thumbs a')	.attr('title',selected.title)
+											.css('background-image',selected.thumb)
+											.html('');
 		}
 	}
 );
--- widget/image/assets/widget.tpl.php
+++ widget/image/assets/widget.tpl.php
@@ -1,11 +1,28 @@
-<div class="box span2">
+<?php
+$hasValue	=(bool)$tpl->get('value');
+if ($hasValue)
+{
+	$parts		=explode('/',$tpl->get('value'));
+	$name		=array_pop($parts);
+	$thumb		=implode('/',$parts).'/_thumbs/120x120/'.$name;
+}
+?>
+<div data-id="<?php $tpl->dataId; ?>" class="box span2">
+	<input type="hidden" name="<?php $tpl->name; ?>" value="<?php $tpl->value; ?>">
 	<div class="box-header">
 		<span class="title"><a href="javascript:{}" data-action="widget.image.main.browseImage" title="Image Not Selected"><i class="icon-picture"></i> Select Image</a></span>
 	</div>
-	<div class="box-content padded imageSelector">
+	<div class="box-content padded imageSelector <?php print $hasValue?'selected':''; ?>">
 		<div class="thumbs">
+			<?php
+			if ($hasValue):
+			?>
+			<a href="javascript:{}" data-action="widget.image.main.browseImage" title="<?php print $name; ?>" style="background-image:url('<?php print $thumb; ?>')"></a>
+			<?php else: ?>
 			<a href="javascript:{}" data-action="widget.image.main.browseImage" title="Image Not Selected"><i class="icon-folder-open icon"></i></a>
-			<!-- <a href="" style="background-image:url(http://farm8.staticflickr.com/7013/6754656011_3de2cc73a2_m.jpg)" title="Lion Rock"></a> -->
+			<?php
+			endif;
+			?>
 		</div>
 	</div>
 </div>
