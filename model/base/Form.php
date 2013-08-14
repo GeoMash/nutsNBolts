fe51054bbc8ec1dee53d9b183610a925355d9914
Timothy Chandler <tim@praxisbt.com>
2013-08-14 22:11:05 +0800

    Updated models.

--- model/base/Form.php
+++ model/base/Form.php
@@ -22,6 +22,7 @@ namespace application\nutsnbolts\model\base
 		public $columns = array
 		(
 			'id' => 'int(10) NOT NULL ' ,
+			'site_id' => 'int(10) NOT NULL ' ,
 			'ref' => 'varchar(100) NOT NULL ' ,
 			'name' => 'varchar(100) NOT NULL ' ,
 			'description' => 'varchar(255) NOT NULL ' ,
