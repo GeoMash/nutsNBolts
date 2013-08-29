2246b912bf95e89914035203982b30720d6c315c
Timothy Chandler <tim@praxisbt.com>
2013-08-30 00:58:26 +0800

    Removed another node tag status check.

--- controller/Index.php
+++ controller/Index.php
@@ -447,7 +447,7 @@ namespace application\nutsNBolts\controller
 
 		public function getNodesByTag($tags,$limit)
 		{
-			$tags=$this->model->NodeTag->read(array('tag'=>$tags,'status'=>1),array(),'LIMIT '.$limit);
+			$tags=$this->model->NodeTag->read(array('tag'=>$tags),array(),'LIMIT '.$limit);
 			return $tags;
 		}
 	}
