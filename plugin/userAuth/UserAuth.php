cc2febf1f4bc0425d7fd4f7c18ac759de4d7f71b
mehrdad.dastgir <mehrdad@praxisbt.com>
2013-10-01 16:25:44 +0800

    added the userAuth plugin files

---
+++ plugin/userAuth/UserAuth.php
@@ -0,0 +1,249 @@
+<?php
+namespace application\wtp\plugin\userAuth
+{
+	use nutshell\Nutshell;
+	use nutshell\core\plugin\Plugin;
+	use nutshell\behaviour\Native;
+	use nutshell\behaviour\Singleton;
+	use application\wtp\model\User;
+	use application\wtp\plugin\userAuth\UserAuthException;
+
+	
+	/**
+	 * This class implements application plugin allowing models to be used.
+	 * Application plugins don't have dependencies nor behaviours.
+	 */
+	class UserAuth extends Plugin implements Native,Singleton 
+	{
+
+		// protected $vModelLoader = null;
+		
+		public static function loadDependencies()
+		{
+			// require_once('AppPluginExtension.php');
+		}
+		
+		public static function registerBehaviours()
+		{
+		
+		}
+		
+		public function init()
+		{
+			
+		}
+
+		private function handleRecord($record)
+		{
+			if (!isset($record['status']))$record['status']=0;
+			
+			if (!empty($record['password']))
+			{
+				$this->generateSalt($record);
+				$record['password']=md5(Nutshell::getInstance()->config->application->salt.$record['salt'].$record['password']);
+			}
+			
+			//For Updates
+			if (isset($record['id']) && is_numeric($record['id']))
+			{
+				if (empty($record['password']))
+				{
+					unset($record['password']);
+				}
+				// var_dump($record);exit();
+				$roles	=$this->extractRoles($record);
+				$return	=$this->plugin->Mvc->model->User->update($record,array('id'=>$record['id']));
+				$this->plugin->Mvc->model->UserRole->delete(array('user_id'=>$record['id']));
+				
+				for ($i=0,$j=count($roles); $i<$j; $i++)
+				{
+					$this->plugin->Mvc->model->UserRole->insert($roles[$i]);
+				}
+				return $return;
+			}
+			//For Inserts
+			else
+			{
+				unset($record['id']);
+				$record['date_created']		=date('Y-m-d H:i:s');
+				$record['date_lastlogin']	='0000-00-00 00:00:00';
+				$record['date_lastactive']	='0000-00-00 00:00:00';
+				
+				$roles=$this->extractRoles($record);
+				if ($id=$this->plugin->Mvc->model->User->insertAssoc($record))
+				{
+					for ($i=0,$j=count($roles); $i<$j; $i++)
+					{
+						$roles[$i]['user_id']=$id;
+						$this->plugin->Mvc->model->UserRole->insertAssoc($roles[$i]);
+					}
+					return $id;
+				}
+			}
+			return false;
+		}
+
+		private function extractRoles(&$record)
+		{
+			if (isset($record['role']))
+			{
+				$roles=array();
+				$id=(!empty($record['id']))?$record['id']:0;
+				foreach ($record['role'] as $roleID=>$enabled)
+				{
+					$roles[]=array
+					(
+						'user_id'	=>$id,
+						'role_id'	=>$roleID
+					);
+				}
+				unset($record['role']);
+				return $roles;
+			}
+			return array();
+		}
+		
+		private function generateSalt(&$record)
+		{
+			$record['salt']=sha1('wheretoparty_ce1833cca4627da0751a2dcdde1f0b3b_'.time());
+		}
+
+		public function addUser(array $array)
+		{
+			try
+			{
+				if (!$array['email'] || !$array['password'] || !$array['password_confirm'])
+				{
+					throw new UserAuthException(UserAuthException::REQUIRED_FIELD_MISSING);
+				}
+				else
+				{
+					if ($array['password']!=$array['password_confirm'])
+					{
+						throw new UserAuthException(UserAuthException::PASSWORDS_DO_NOT_MATCH);
+					}
+					
+					unset($array['password_confirm']);
+					
+					if($this->handleRecord($array))
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			catch(NutshellException $e)
+			{
+				throw new UserAuthException(UserAuthException::REQUEST_FAILED, $this->plugin->Logger()->warn($e->__toString()));
+			}
+			
+		}
+
+		public function editUser(array $array)
+		{
+			if ($array['email'])
+			{
+				if ($array['password']!=$array['password_confirm'])
+				{
+					throw new UserAuthException(
+						UserAuthException::PASSWORDS_DO_NOT_MATCH,
+						$this->plugin->Logger()->warn($e->__toString())
+						);
+				}
+
+				unset($array['password_confirm']);
+
+				if ($this->handleRecord($array)!==false)
+				{
+					return $this->plugin->Mvc->model->User->read($array['id']);
+				}
+				else
+				{
+					throw new UserAuthException(
+						UserAuthException::REQUEST_FAILED, 
+						$this->plugin->Logger()->warn($e->__toString())
+						);
+				}
+			}
+		}
+
+		public function generateUserList()
+		{
+			return $this->plugin->Mvc->model->User->read();
+
+		}
+
+		public function generateRolesList($userId=null)
+		{
+			$return='';
+			if (is_numeric($userId))
+			{
+				$userRoles=$this->plugin->Mvc->model->UserRole->read(array('user_id'=>$userId));
+			}
+			$roles	=$this->plugin->Mvc->model->Role->read();
+			$html	=array();
+			for ($i=0,$j=count($roles); $i<$j; $i++)
+			{
+				$checked='';
+				if (isset($userRoles))
+				{
+					$checked=($this->userHasRole($userRoles,$roles[$i]['id']))?'checked':'';
+				}
+				$html[]=<<<HTML
+<tr>
+	<td class=""><input type="checkbox" name="role[{$roles[$i]['id']}]" value="1" {$checked}></td>
+	<td class="">{$roles[$i]['name']}</td>
+	<td class="">{$roles[$i]['description']}</td>
+</tr>
+HTML;
+			}
+			$return=implode('',$html);
+			return $return;
+		}
+		
+		public function userHasRole($userRoles,$roleID)
+		{
+			for ($i=0,$j=count($userRoles); $i<$j; $i++)
+			{
+				if ($userRoles[$i]['role_id']==$roleID)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public function authenticate($email,$password)
+		{
+			$user		=$this->plugin->Mvc->model->User->read(array('email'=>$email));
+			if (isset($user[0]))
+			{
+				$userSalt	=$user[0]['salt'];
+				$systemSalt	=Nutshell::getInstance()->config->application->salt;
+				
+				$result	=$this->plugin->Mvc->model->User->read
+				(
+					array
+					(
+						'email'		=>$email,
+						'pin'	=>md5($systemSalt.$userSalt.$password),
+						'status'	=>1
+					)
+				);
+				if (isset($result[0]))
+				{
+					return $result[0];
+				}
+			}
+			return false;
+		}
+
+		// public function seeSalt()
+		// {
+		// 	var_dump($this->config->salt);
+		// }
+	}
+}
---
+++ plugin/userAuth/UserAuthException.php
@@ -0,0 +1,18 @@
+<?php
+namespace application\wtp\plugin\userAuth
+{
+	use nutshell\core\exception\NutshellException;
+
+	
+	class UserAuthException extends NutshellException
+	{
+		/** The request must be valid JSON */
+		const PASSWORDS_DO_NOT_MATCH			= 1;
+		
+		const REQUEST_FAILED = 2;
+
+		const REQUIRED_FIELD_MISSING=3;
+		
+	}
+}
+?>
