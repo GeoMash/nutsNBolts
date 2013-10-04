4821431165c6647bb0cec7273f9812146020edf0
Timothy Chandler <tim@praxisbt.com>
2013-10-05 00:21:48 +0800

    Initial commit for workflow plugin.

---
+++ plugin/workflow/Workflow.php
@@ -0,0 +1,16 @@
+<?php
+namespace application\nutsNBolts\plugin\workflow
+{
+	use nutshell\behaviour\Singleton;
+	use nutshell\core\plugin\Plugin;
+
+	class Workflow extends Plugin implements Singleton
+	{
+		public function init()
+		{
+
+		}
+
+
+	}
+}
