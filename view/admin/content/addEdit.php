8143c5ebb03d09e87c36e8e2bbaae6816421485d
mehrdad.dastgir <mehrdad@praxisbt.com>
2013-10-04 22:09:53 +0800

    added permission check for the path

--- view/admin/content/addEdit.php
+++ view/admin/content/addEdit.php
@@ -31,41 +31,43 @@
 									</div>
 								</div>
 							</div>
-							<div class="container-fluid padded">
-								<div class="box">
-									<div class="box-header">
-										<span class="title"><i class="icon-globe"></i> URL Paths</span>
-									</div>
-									<div class="content-box">
-										<div class="padded">
-											<div class="well relative">
-												<table class="table table-hover">
-													<thead>
-														<th class="span4">Path</th>
-														<th>Remove</th>
-													</thead>
-													<tbody>
-														<?php
-														$urls=$tpl->get('nodeURLs');
-														for ($i=0,$j=count($urls); $i<$j; $i++):
-														?>
-														<tr>
-															<td>
-																<input type="text" class="input-medium" value="<?php print $urls[$i]['url']; ?>" data-prompt-position="topLeft" class="validate[required]" name="url[]">
-															</td>
-															<td>
-																<button data-action="removeURLfromContent" class="btn btn-danger btn-mini" type="button">&times;</button>
-															</td>
-														</tr>
-													<?php endfor; ?>
-													</tbody>
-												</table>
-												<button type="button" class="btn btn-green" data-action="addURLToContent"><i class="icon-plus"></i> Add URL Path</button>
+							<?php if ($tpl->challangeRole(array('SUPER','ADMIN','CONTENT_CREATOR','CONTENT_EDITOR'))): ?>
+								<div class="container-fluid padded">
+									<div class="box">
+										<div class="box-header">
+											<span class="title"><i class="icon-globe"></i> URL Paths</span>
+										</div>
+										<div class="content-box">
+											<div class="padded">
+												<div class="well relative">
+													<table class="table table-hover">
+														<thead>
+															<th class="span4">Path</th>
+															<th>Remove</th>
+														</thead>
+														<tbody>
+															<?php
+															$urls=$tpl->get('nodeURLs');
+															for ($i=0,$j=count($urls); $i<$j; $i++):
+															?>
+															<tr>
+																<td>
+																	<input type="text" class="input-medium" value="<?php print $urls[$i]['url']; ?>" data-prompt-position="topLeft" class="validate[required]" name="url[]">
+																</td>
+																<td>
+																	<button data-action="removeURLfromContent" class="btn btn-danger btn-mini" type="button">&times;</button>
+																</td>
+															</tr>
+														<?php endfor; ?>
+														</tbody>
+													</table>
+													<button type="button" class="btn btn-green" data-action="addURLToContent"><i class="icon-plus"></i> Add URL Path</button>
+												</div>
 											</div>
 										</div>
 									</div>
 								</div>
-							</div>
+							<?php endif; ?>
 							<div class="container-fluid padded">
 								<div class="box">
 									<div class="box-header">
