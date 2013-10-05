a89be3a0e3727a7ed3a6d0f7459965d0dc19499f
Timothy Chandler <tim@praxisbt.com>
2013-10-05 14:12:58 +0800

    Hid the publish button.

--- view/admin/content/addEdit.php
+++ view/admin/content/addEdit.php
@@ -7,12 +7,14 @@
 						<div class="box-header">
 							<span class="title"><i class="<?php $tpl->contentTypeIcon; ?>"></i> <?php $tpl->contentType; ?></span>
 							<ul class="box-toolbar">
+								<!--
 								<li>
 									<span>Published: </span>
 								</li>
 								<li>
 									<input type="checkbox" class="iButton-icons" <?php print (bool)$tpl->get('status')?'checked':''; ?> name="status" value="1" />
 								</li>
+								-->
 							</ul>
 						</div>
 						<div class="box-content">
