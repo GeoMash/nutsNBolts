78be2713968459c7568e3bc88febb13f1f7e109c
Timothy Chandler <tim@praxisbt.com>
2013-10-09 01:15:53 +0800

    Updated the upgrade SQL file.

--- doc/upgrade/1.0.0-1.1.0.sql
+++ doc/upgrade/1.0.0-1.1.0.sql
@@ -23,24 +23,35 @@ CREATE TABLE `workflow_step` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `workflow_step_action` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `step_id` int(10) NOT NULL,
-  `action_id` int(10) NOT NULL,
-  `direction` tinyint(1) NOT NULL,
-  `params` text NOT NULL,
-  `order` int(5) NOT NULL DEFAULT '0',
+  `transition_id` int(10) NOT NULL,
+  `from_step_id` int(10) NOT NULL,
+  `to_step_id` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `workflow_step_transition` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `from_step_id` int(10) NOT NULL,
-  `to_step_id` int(10) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `step_transition_id` int(10) NOT NULL,
+  `action_id` int(10) NOT NULL,
+  `params` text NOT NULL,
+  `order` int(5) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `workflow_transition` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 CREATE TABLE `workflow_transition_role` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `transition_id` int(10) NOT NULL,
   `role_id` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `collection_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `collection_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
