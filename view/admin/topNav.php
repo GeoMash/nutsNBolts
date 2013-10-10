0763dad31c25e332234cbdc66e4fc85bb87adc61
mehrdad.dastgir <mehrdad@praxisbt.com>
2013-10-11 01:59:15 +0800

    added a placeholder image for the profile pictures

--- view/admin/topNav.php
+++ view/admin/topNav.php
@@ -20,7 +20,11 @@ $user=$tpl->get('user');
 
 				<li class="with-image">
 					<div class="avatar">
+					<?php if(isset($user['image']) && strlen($user['image']>0)): ?>
 						<img src="<?php echo $user['image']; ?>" />
+					<?php else: ?>
+						<img src="/images/avatars/silhouette.png" />
+					<?php endif; ?>
 					</div>
 					<span><?php print $user['name_first'].' '.$user['name_last']; ?></span>
 				</li>
