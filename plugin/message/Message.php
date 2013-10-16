28e46e934af2fc4fa69b3e30ddae71ade7343fc2
Timothy Chandler <tim@praxisbt.com>
2013-10-16 17:54:52 +0800

    Fixed query to use user id.

--- plugin/message/Message.php
+++ plugin/message/Message.php
@@ -36,13 +36,15 @@ namespace application\nutsNBolts\plugin\message
 
 		public function getUnreadMessageCount()
 		{
+			$userId=$this->plugin->UserAuth->getUserId();
 			$return=0;
 			$query=<<<SQL
 SELECT COUNT(id) AS total
 FROM message
-WHERE status=0
+WHERE to_user_id=?
+AND status=0
 SQL;
-			if ($this->db->select($query))
+			if ($this->db->select($query,$userId))
 			{
 				$result=$this->db->result('assoc');
 				if (isset($result[0]))
