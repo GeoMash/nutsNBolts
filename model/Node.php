03dfa58f49fa0eb91103bd8734eae04ec236c1ae
mehrdad dastgir <mehrdad@mypropertydata.com.my>
2013-10-29 01:33:29 +0800

    fixed the blog query

--- model/Node.php
+++ model/Node.php
@@ -269,8 +269,8 @@ SQL;
 					$nodes[$records[$i]['id']][$records[$i]['ref']]=$records[$i]['value'];
 				}
 				//Reset index.
-				print_r($nodes);
-				die('halt');
+				// print_r($nodes);
+				// die('halt');
 				sort($nodes);
 				return $nodes;
 			}
@@ -381,7 +381,7 @@ SQL_PART;
 SQL_PART;
 			}
 			$query=<<<SQL
-			SELECT node.*,content_part.label,content_part.ref,node_part.value,content_type_user.user_id
+			SELECT node.*,content_part.ref,node_part.value,content_type_user.user_id
 			FROM node
 			LEFT JOIN node_part ON node.id=node_part.node_id
 			LEFT JOIN content_part ON node_part.content_part_id=content_part.id
@@ -417,6 +417,8 @@ SQL;
 					$nodes[$records[$i]['id']][$records[$i]['ref']]=$records[$i]['value'];
 				}
 				//Reset index.
+				// print_r($nodes);
+				// die();
 				sort($nodes);
 				return $nodes;
 			}
