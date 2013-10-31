6c1ac2b433c21c968633c3df8836caf157e48550
mehrdad.dastgir <mehrdad@praxisbt.com>
2013-10-31 17:12:27 +0800

    modified the base controller to allow for widget and hooks loading

--- base/Controller.php
+++ base/Controller.php
@@ -12,7 +12,8 @@ namespace application\nutsNBolts\base
 		{
 			parent::__construct($MVC);
 			$this->MVC=$MVC;
-			
+			$path			=$this->getPath();
+			$page			=$this->model->PageMap->getPageFromPath($path);			
 			$result=$this->model->Site->read(array('domain'=>$_SERVER['HTTP_HOST']));
 			if (isset($result[0]))
 			{
@@ -26,6 +27,19 @@ namespace application\nutsNBolts\base
 			{
 				die('No site bound for this domain!');
 			}
+			
+			
+			$page			=$this->model->PageMap->getPageFromPath($path);
+			
+
+			// if(isset($page['ref']))
+			// {
+				
+				// $pageRef=str_replace('/', '_', 'admin/configurecontent/types/add');
+				// $this->loadHooks('admin/settings/users/add');
+				// $this->loadCustomWidgets($pageRef);
+			// }
+						
 		}
 		
 		public function getSite()
@@ -48,6 +62,155 @@ namespace application\nutsNBolts\base
 			header('location:'.$path);
 			exit();
 		}
+		
+		public function loadHooks($ref=null)
+		{
+			$ref=ucfirst($ref);
+			foreach ($this->application->getLoaded() as $applicationRef=>$application)
+			{
+				$this->loadGlobalHooks($applicationRef);
+				$this->loadFormHooks($applicationRef);
+				if (!$ref)continue;
+				$className=$this->application->getNamespace($applicationRef).'\hook\\'.$ref;
+				$path=APP_HOME.lcfirst($applicationRef)._DS_.'hook'._DS_.$ref.'.php';
+				if (is_file($path))
+				{
+					require_once($path);
+					if (class_exists($className))
+					{
+						if (!is_array($this->hookContainers[$applicationRef]))
+						{
+							$this->hookContainers[$applicationRef]=array();
+						}
+						$this->hookContainers[$applicationRef][$ref]=new $className($this->model,$this->view);
+					}
+					else
+					{
+						//TODO: Throw exception re bad hook class name
+					}
+				}
+			}
+		}	
+		
+		private function loadGlobalHooks($applicationRef)
+		{
+			$className=$this->application->getNamespace($applicationRef).'\hook\_Global';
+			$path=APP_HOME.lcfirst($applicationRef)._DS_.'hook'._DS_.'_Global.php';
+			if (is_file($path))
+			{
+				require_once($path);
+				if (class_exists($className))
+				{
+					if ( isset($this->hookContainers[$applicationRef]) && !is_array($this->hookContainers[$applicationRef]))
+					{
+						$this->hookContainers[$applicationRef]=array();
+					}
+					$this->hookContainers[$applicationRef]['_Global']=new $className($this->model,$this->view);
+				}
+				else
+				{
+					//TODO: Throw exception re bad hook class name
+				}
+			}
+		}
+				
+		private function loadFormHooks($applicationRef)
+		{
+			$className=$this->application->getNamespace($applicationRef).'\hook\_Forms';
+			$path=APP_HOME.lcfirst($applicationRef)._DS_.'hook'._DS_.'_Forms.php';
+			if (is_file($path))
+			{
+				require_once($path);
+				if (class_exists($className))
+				{
+					if ( isset($this->hookContainers[$applicationRef]) && !is_array($this->hookContainers[$applicationRef]))
+					{
+						$this->hookContainers[$applicationRef]=array();
+					}
+					$this->hookContainers[$applicationRef]['_Forms']=new $className($this->model,$this->view);
+				}
+				else
+				{
+					//TODO: Throw exception re bad hook class name
+				}
+			}
+		}
+
+		public function execHook($hook,&$a=null,&$b=null,&$c=null,&$d=null,&$e=null,&$f=null,&$g=null,&$h=null,&$i=null,&$j=null,&$k=null)//($hook,&$args)
+		{
+			$table	=array_merge(array('hook'),range('a','k'));
+			$xargs	=array();
+			$args	=func_get_args();
+			for ($ci=1,$cj=func_num_args(); $ci<$cj; $ci++)
+			{
+				$xargs[]=&$args[$ci];
+			}
+			foreach ($this->application->getLoaded() as $applicationRef=>$application)
+			{
+				if (!isset($this->hookContainers[$applicationRef]))
+				{
+					continue;
+				}
+				foreach ($this->hookContainers[$applicationRef] as $container)
+				{
+					if (method_exists($container,$hook))
+					{
+						call_user_func_array(array($container,$hook),$xargs);
+					}
+				}
+			}
+			for ($ci=1,$cj=func_num_args(); $ci<$cj; $ci++)
+			{
+				${$table[$ci]}=$xargs[$ci-1];
+			}
+		}		
+		
+		private function getPath()
+		{
+			$nodes=$this->request->getNodes();
+			if (count($nodes)===1 && empty($nodes[0]))
+			{
+				return '/';
+			}
+			else
+			{
+				return '/'.implode('/',$nodes).'/';
+			}
+		}	
+		
+		private $widgetContainers=array();
+		public function loadCustomWidgets($ref=null)
+		{	
+			
+			$ref=ucfirst($ref);
+			foreach ($this->application->getLoaded() as $applicationRef=>$application)
+			{
+				if($applicationRef != "NutsNBolts")
+				{
+					if (!$ref)continue;
+					$className=$this->application->getNamespace($applicationRef).'\widget\\'.strtolower($ref).'\\'.$ref;
+					$path=APP_HOME.lcfirst($applicationRef)._DS_.'widget'._DS_.strtolower($ref)._DS_.$ref.'.php';
+					if (is_file($path))
+					{
+						require_once($path);	
+						if (class_exists($className))
+						{
+							if (!is_array($this->widgetContainers))
+							{
+								$this->widgetContainers[$applicationRef]=array();
+							}
+							$this->widgetContainers[$applicationRef][$ref]=new $className($this->model,$this->view);
+						}
+						else
+						{
+							//TODO: Throw exception re bad widget class name
+						}
+					}					
+				}
+
+			}
+		}			
+					
 	}
 }
 ?>
