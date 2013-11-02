45269168f4fbc485ca9ed363aca71eeab18bdd5c
mehrdad.dastgir <mehrdad@praxisbt.com>
2013-11-03 02:15:20 +0800

    removed un-needed init

--- base/AdminController.php
+++ base/AdminController.php
@@ -111,10 +111,6 @@ namespace application\nutsNBolts\base
 						print $this->unreadMessages;
 					}
 				);
-			if (method_exists($this,'init'))
-			{
-				$this->init();
-			}
 		}
 		
 		public function show404()
