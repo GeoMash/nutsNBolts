6165ad71d16f959ea0943f20175cfeb9ac520515
Timothy Chandler <tim@praxisbt.com>
2013-11-08 02:05:22 +0800

    Sandboxing.

--- controller/Sandbox.php
+++ controller/Sandbox.php
@@ -4,15 +4,18 @@ namespace application\nutsNBolts\controller
 	use nutshell\Nutshell;
 	use nutshell\core\exception\NutshellException;
 	use nutshell\plugin\mvc\Controller;
+	use application\nutsNBolts\Encoding;
 
 	class Sandbox extends Controller
 	{
 		public function index()
 		{
-			$SMS=$this->plugin->Sms('M3Tech');
-			$SMS->setMobileNumber('60172359029')
-				->setMessage('Test test test')
-				->send();
+			$nodes=$this->model->Node->getWithParts(array('content_part_id'=>324));
+			for ($i=0,$j=count($nodes); $i<$j; $i++)
+			{
+				var_dump($nodes[$i]);
+				break;
+			}
 		}
 	}
 }
