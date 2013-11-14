5ec80be4c67f2c6aafc58977553d2e2f3076b0fd
Timothy Chandler <tim@praxisbt.com>
2013-11-15 01:39:22 +0800

    for rename

---
+++ widget/wysiwyg.rename/Wysiwyg.php
@@ -0,0 +1,14 @@
+<?php
+namespace application\nutsNBolts\widget\wysiwyg
+{
+	use application\nutsNBolts\widget\base\Widget as WidgetBase;
+
+	class Wysiwyg extends WidgetBase
+	{
+		public function init()
+		{
+			
+		}
+	}
+}
+?>
