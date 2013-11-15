2146ad50e805a0a8f7181ffb2a81d2a381ca3d34
mehrdad.dastgir <mehrdad@praxisbt.com>
2013-11-15 15:39:04 +0800

    semi colon in the wrong location

--- model/Node.php
+++ model/Node.php
@@ -210,8 +210,8 @@ SQL_PART;
 				LEFT JOIN content_part ON node_part.content_part_id=content_part.id
 				WHERE {$where}
 			)
-			ORDER BY node.id ASC;
-			{$limitSql}
+			ORDER BY node.id ASC
+			{$limitSql};
 SQL;
             if ($result=$this->plugin->Db->nutsnbolts->select($query))
             {
