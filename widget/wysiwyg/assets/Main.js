e91aa36d7357305a89786b999386597de00d7eb1
Timothy Chandler <tim@praxisbt.com>
2013-11-18 01:15:11 +0800

    Renders wysiwyg in correct order.

--- widget/wysiwyg/assets/Main.js
+++ widget/wysiwyg/assets/Main.js
@@ -8,9 +8,63 @@ $JSKK.Class.create
 (
 	{},
 	{
-		init: function(id)
+		init: function(id,config)
 		{
-//			$('[data-id="'+id+'"]').
+			var toolbarOrder=
+				[
+					'basicstyles',
+					'paragraph',
+					'/',
+					'styles',
+					'links',
+					'insert',
+					'/',
+					'editing',
+					'clipboard',
+					'tools',
+				    'document'
+				],
+				toolbar=
+				[
+
+				];
+			for (var i=0,j=toolbarOrder.length; i<j; i++)
+			{
+				if (toolbarOrder[i]!='/')
+				{
+					toolbar[i]=
+					{
+						name:   toolbarOrder[i],
+						items:  config[toolbarOrder[i]]
+					}
+				}
+				else
+				{
+					toolbar[i]='/';
+				}
+			}
+			CKEDITOR.replace
+			(
+				'wysiwyg-'+id,
+				{
+					toolbar:toolbar
+//					[
+//						{ name: 'document', items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
+//						{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+//						{ name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+//						'/',
+//						{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+//						{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'language' ] },
+//						{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+//						{ name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+//						'/',
+//						{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+//						{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+//						{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+//						{ name: 'others', items: [ '-' ] },
+//					]
+				}
+			);
 		}
 	}
 );
