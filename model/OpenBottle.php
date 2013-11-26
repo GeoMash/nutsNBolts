f6de7434e7f289a9ecab7d1a8779d02c7902399c
mehrdad.dastgir <mehrdad@praxisbt.com>
2013-11-26 17:17:36 +0800

    added a new query

--- model/OpenBottle.php
+++ model/OpenBottle.php
@@ -82,9 +82,24 @@ FROM open_bottle
 WHERE bottle_id=?
 AND user_id=?
 SQL;
-            echo $query;
-            die();
             $result=$this->plugin->Db->nutsnbolts->getResultFromQuery($query,array($bottleId,$userId));
+            return $result;
+        }
+
+        public function managerRead($barId)
+        {
+            $barId=$barId['bar_id'];
+            $query=<<<SQL
+SELECT *
+FROM open_bottle t
+WHERE date_opened = (
+    SELECT max(date_opened)
+    FROM open_bottle
+    WHERE t.parent_id = parent_id
+    AND bar_id=?
+)
+SQL;
+            $result=$this->plugin->Db->nutsnbolts->getResultFromQuery($query,array($barId));
             return $result;
         }
 
