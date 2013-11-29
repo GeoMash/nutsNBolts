d61d8d63311312c3c331b9b44049dacfa8970377
mehrdad.dastgir <mehrdad@praxisbt.com>
2013-11-29 11:40:17 +0800

    copied over Tim's fix for the profile page from a different branch

--- controller/admin/Profile.php
+++ controller/admin/Profile.php
@@ -1,60 +1,32 @@
 <?php
 namespace application\nutsNBolts\controller\admin
 {
-	use application\nutsNBolts\base\AdminController;
-	use nutshell\helper\ObjectHelper;
-	
-	class Profile extends AdminController
-	{
-		public function init()
-		{
-			if(strlen($this->request->node(2))>0)
-			{
-				$action=$this->request->node(2);
-			}
-			else
-			{
-				$action='view';
-			}
-			
-			switch ($action)
-			{
-				case 'view':
-					$this->view();
-				break;
-				
-				case 'edit':
-					$this->edit();
-				break;
-			}
-			
-		}
-		private function view()
-		{
-			$userId=$this->plugin->UserAuth->getUserId();
-			$this->setContentView('admin/profile');
-			$this->view->setVar('userDetails',$this->plugin->Mvc->model->User->read($userId));
-			$this->addBreadcrumb('Profile','icon-edit','profile');
-			// $this->view->render();			
-		}
-		
-		private function edit()
-		{
-			$userId=$this->plugin->UserAuth->getUserId();
-			if ($userId)
-			{
-				$record=$this->request->getAll();
-				if($this->plugin->Mvc->model->User->handleRecord($record))
-				{
-					$this->view();	
-				}
-				
-			}
-			else
-			{
-				$this->view();
-			}
-		}		
-	}
+    use application\nutsNBolts\base\AdminController;
+    use nutshell\helper\ObjectHelper;
+
+    class Profile extends AdminController
+    {
+        public function index()
+        {
+            $userId=$this->plugin->UserAuth->getUserId();
+            $this->setContentView('admin/profile');
+            $this->view->setVar('userDetails',$this->plugin->Mvc->model->User->read($userId));
+            $this->addBreadcrumb('Profile','icon-user','profile');
+            $this->view->render();
+        }
+
+        public function edit()
+        {
+            if($this->plugin->Mvc->model->User->handleRecord($this->request->getAll()))
+            {
+                $this->plugin->Notification->setSuccess('Profile updated.');
+            }
+            else
+            {
+                $this->plugin->Notification->setError('Oops! Something went wrong, and this is a terrible error message!');
+            }
+            $this->redirect('/admin/profile/');
+        }
+    }
 }
 ?>
