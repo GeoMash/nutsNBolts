d5220949afbaf8fa9ca6e65ac33af2858df7ae38
ali <ali@praxisbt.com>
2014-03-17 13:07:20 +0800

    Add Index.php file for userRole

--- controller/rest/userRole/Index.php
+++ controller/rest/userRole/Index.php
@@ -1,7 +1,59 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: GeoMash
- * Date: 3/17/14
- * Time: 12:54 PM
- */ 
+namespace application\nutsNBolts\controller\rest\userRole
+{
+	use application\nutsNBolts\base\RestController;
+
+	class Index extends RestController
+	{
+		public function init()
+		{
+			$this->bindPaths
+			(
+				array
+				(
+					'{int}'				=>'getByUserId'
+				)
+			);
+		}
+		
+		public function getByUserId()
+		{
+			$userId=$this->getFullRequestPart(2);
+			if(isset($userId) && is_numeric($userId))
+			{
+				$user=$this->model->UserRole->read(['user_id'=>$userId]);
+				if(count($user))
+				{
+					$this->setResponseCode(200);
+					$this->respond
+					(
+						true,
+						'OK',
+						$user[0]
+					);					
+				}
+				else
+				{
+					// user not found
+					$this->setResponseCode(404);
+					$this->respond
+					(
+						true,
+						'Not Found',
+						'User not found'
+					);					
+				}
+			}
+			else
+			{
+				$this->setResponseCode(404);
+				$this->respond
+				(
+					false,
+					'Expecting user id to be an integer.'
+				);
+			}
+		}	
+	}
+}
+?>
