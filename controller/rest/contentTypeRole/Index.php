92871392dc8f91581b761600c159fcaf55ec3528
ali <ali@praxisbt.com>
2014-03-17 17:52:02 +0800

    Add contentTypeRole to rest controller

--- controller/rest/contentTypeRole/Index.php
+++ controller/rest/contentTypeRole/Index.php
@@ -1,7 +1,61 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: GeoMash
- * Date: 3/17/14
- * Time: 4:13 PM
- */ 
+namespace application\nutsNBolts\controller\rest\contentTypeRole
+{
+	use application\nutsNBolts\base\RestController;
+
+	class Index extends RestController
+	{
+		public function init()
+		{
+			$this->bindPaths
+			(
+				array
+				(
+					'{int}'				=>'getByContentId',
+				)
+			);
+		}
+		/*
+		 * sample request: $.getJSON('/rest/contentType/1.json');
+		 */
+		public function getByContentId()
+		{
+			$contentTypeId=$this->getFullRequestPart(2);
+			if(isset($contentTypeId) && is_numeric($contentTypeId))
+			{
+				$content=$this->model->ContentTypeRole->read(['content_type_id'=>$contentTypeId]);
+				if(count($content))
+				{
+					$this->setResponseCode(200);
+					$this->respond
+					(
+						true,
+						'OK',
+						$content[0]
+					);					
+				}
+				else
+				{
+					// no content by that id
+					$this->setResponseCode(404);
+					$this->respond
+					(
+						true,
+						'Not Found',
+						'Content type role not found'
+					);					
+				}
+			}
+			else
+			{
+				$this->setResponseCode(404);
+				$this->respond
+				(
+					false,
+					'Expecting content type id to be an integer.'
+				);
+			}
+		}
+	}
+}
+?>
