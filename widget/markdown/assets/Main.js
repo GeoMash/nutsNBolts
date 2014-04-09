2e1c466c97e66be42c62a7318e7ce05cf4e1465a
Timothy Chandler <tim@praxisbt.com>
2014-04-09 15:14:04 +0800

    Implemented the new markdown editor widget.

--- widget/markdown/assets/Main.js
+++ widget/markdown/assets/Main.js
@@ -8,9 +8,14 @@ $JSKK.Class.create
 (
 	{},
 	{
-		init: function(id,config)
+		converter:	null,
+		editor:		null,
+		init:		function(id,config)
 		{
-			
+			this.converter	=Markdown.getSanitizingConverter();
+			this.editor		=new Markdown.Editor(this.converter,id);
+			this.editor.run();
+			$('#'+id+'-input').autosize();
 		}
 	}
 );
--- widget/markdown/assets/widget.tpl.php
+++ widget/markdown/assets/widget.tpl.php
@@ -1,3 +1,21 @@
-<textarea id="wysiwyg-<?php $tpl->dataId; ?>" data-id="<?php $tpl->dataId; ?>" name="<?php $tpl->name; ?>">
-<?php $tpl->value; ?>
-</textarea>
+<div class="row-fluid">
+	<div class="span6">
+		<div class="box">
+			<div class="box-header">
+				<span class="title"><i class="icon-pencil"></i> Markdown Editor</span>
+			</div>
+			<div class="box-content padded">
+				<div id="<?php $tpl->dataId; ?>-button-bar"></div>
+				<textarea id="<?php $tpl->dataId; ?>-input" class="wmd-input" data-id="<?php $tpl->dataId; ?>" name="<?php $tpl->name; ?>"><?php $tpl->value; ?></textarea>
+			</div>
+		</div>
+	</div>
+	<div class="span6">
+		<div class="box">
+			<div class="box-header">
+				<span class="title"><i class="icon-search"></i> Preview</span>
+			</div>
+			<div id="<?php $tpl->dataId; ?>-preview" class="box-content padded"></div>
+		</div>
+	</div>
+</div>
