48fe765f771b4ad7604fb1c27a51459c1546563e
Timothy Chandler <tim@praxisbt.com>
2014-04-17 18:03:50 +0800

    Rewrote the rest URL parsing and execution to work off of a cached map of all possible URLs. Additionally, the URLs which a rest controller can define have been enhanced with a more powerful syntax.

--- controller/rest/Index.php
+++ controller/rest/Index.php
@@ -2,21 +2,38 @@
 namespace application\nutsNBolts\controller\rest
 {
 	use application\nutsNBolts\base\Controller;
+	use RecursiveDirectoryIterator;
+	use RecursiveIteratorIterator;
+	use RegexIterator;
+	use RecursiveRegexIterator;
+	use ReflectionClass;
+	use ReflectionException;
 
 	class Index extends Controller
 	{
+		const CACHE_FILE	='/../../cache/nodeMap.json';
 		const FORMAT_DEFAULT='html';
 
 		private $restController=null;
+		private $pathVars=
+		[
+			'{*}',
+			'[*]',
+			'[...]',
+			'{string}',
+			'[string]',
+			'{int}',
+			'[int]'
+		];
 
 		public function index()
 		{
 			$request		=$this->request->getNodes();
 			$lastIndex		=count($request)-1;
-			$latNodeParts	=explode('.',$request[$lastIndex]);
-			if (isset($latNodeParts[1]) && $latNodeParts[1]!=$request[$lastIndex])
+			$lastNodeParts	=explode('.',$request[$lastIndex]);
+			if (isset($lastNodeParts[1]) && $lastNodeParts[1]!=$request[$lastIndex])
 			{
-				$format=$latNodeParts[1];
+				$format=$lastNodeParts[1];
 				$request[$lastIndex]=str_replace('.'.$format,'',$request[$lastIndex]);
 			}
 			else
@@ -25,40 +42,201 @@ namespace application\nutsNBolts\controller\rest
 			}
 			if (isset($request[1]))
 			{
-				$className=$this->getController($request[1]);
-				if (!empty($className))
+				$classMap=$this->getRestMap();
+				$fail		=false;
+				foreach ($classMap as $path=>$mapItem)
 				{
-					array_shift($request);
-					array_shift($request);
-					$this->restController=new $className($this->MVC,$request,$format);
-					exit();
+					for ($i=0,$j=count($request),$l=count($mapItem['nodes']); ($i<$j || $i<$l); $i++)
+					{
+						if (!isset($request[$i]) || (!isset($mapItem['nodes'][$i]) || $request[$i]!=$mapItem['nodes'][$i]))
+						{
+							$fail=!$this->parsePathVar($request,$mapItem['nodes'],$i);
+						}
+						if ($fail)
+						{
+							unset($classMap[$path]);
+							$fail=false;
+							break;
+						}
+					}
+				}
+				$numResults=count($classMap);
+				if ($numResults>1)
+				{
+					die('Relevance checking has not yet been implemented for multiple mapped results.');
+				}
+				elseif ($numResults===1)
+				{
+					$mapItem=array_values($classMap)[0];
 				}
 				else
 				{
-					//TODO: Handle properly.
-					die('INVALID REQUEST');
+					die('No service handler found for this URL.');
 				}
+				
+				$params=$this->extractParams($mapItem,$request);
+				
+				$this->resultController=new $mapItem['class']($this->MVC,$request,$format);
+				
+				call_user_func_array(array($this->resultController,$mapItem['method']),$params);
+				exit();
+//				$className=$this->getController($request[1]);
+//				if (!empty($className))
+//				{
+//					array_shift($request);
+//					array_shift($request);
+//					$this->restController=new $className($this->MVC,$request,$format);
+//					exit();
+//				}
+//				else
+//				{
+//					//TODO: Handle properly.
+//					die('INVALID REQUEST');
+//				}
 			}
 			//TODO: Handle properly.
 			die('INVALID REQUEST');
 		}
-
-		private function getController($serviceNode)
+		
+		private function extractParams($restDef,$request)
 		{
-			$return		=null;
-			foreach ($this->application->getLoaded() as $applicationRef=>$application)
+			$params=[];
+			for ($i=0,$j=count($request),$l=count($restDef['nodes']); ($i<$j || $i<$l); $i++)
 			{
-				$basePath=APP_HOME.lcfirst($applicationRef)._DS_.'controller'._DS_.'rest'._DS_;
-				if (is_file($basePath.ucfirst($serviceNode).'.php'))
+				if (in_array($restDef['nodes'][$i],$this->pathVars))
 				{
-					$return=$this->application->getNamespace($applicationRef).'\controller\rest\\'.ucfirst($serviceNode);
-				}
-				else if (is_file($basePath.lcfirst($serviceNode)._DS_.'Index.php'))
-				{
-					$return=$this->application->getNamespace($applicationRef).'\controller\rest\\'.lcfirst($serviceNode).'\\'.'Index';
+					$params[]=$request[$i];
 				}
 			}
-			return $return;
+			return $params;
+		}
+		
+		private function parsePathVar($request,$nodes,$index)
+		{
+			$nodeCount=count($nodes);
+			if (isset($nodes[$index]))
+			{
+				$parseVar=$nodes[$index];
+			}
+			else
+			{
+				$parseVar=null;
+			}
+			if ($parseVar=='[...]'
+			|| (count($request)>$nodeCount && $index>=$nodeCount))
+			{
+				if ($parseVar=='[...]' && empty($request[$index]))
+				{
+					return true;
+				}
+				else if (in_array('[...]',$nodes))
+				{
+					$parseVar=$nodes[$nodeCount-2];
+					if ($parseVar=='[...]')//Cannot have more than 1 of these. Invalid path!!!
+					{
+						return false;
+					}
+				}
+			}
+			switch ($parseVar)
+			{
+				case '{*}':
+				{
+					if (empty($request[$index]))
+					{
+						return false;
+					}
+					break;
+				}
+				case '[*]':
+				{
+					break;
+				}
+				case '{int}':
+				{
+					if (!isset($request[$index]) || !is_numeric($request[$index]))
+					{
+						return false;
+					}
+					break;
+				}
+				case '[int]':
+				{
+					if (isset($request[$index]) && !is_numeric($request[$index]))
+					{
+						return false;
+					}
+					break;
+				}
+				case '{string}':
+				{
+					if (!isset($request[$index]) || !is_string($request[$index]))
+					{
+						return false;
+					}
+					break;
+				}
+				case '[string]':
+				{
+					if (isset($request[$index]) && !is_string($request[$index]))
+					{
+						return false;
+					}
+					break;
+				}
+				default:
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		private function getRestMap()
+		{
+			$cacheFile=__DIR__.self::CACHE_FILE;
+//			if (!is_file($cacheFile))
+//			{
+//				$this->buildCacheFile();
+//			}
+			$this->buildCacheFile();
+			return json_decode(file_get_contents($cacheFile),true);
+		}
+		
+		private function buildCacheFile()
+		{
+			$cacheFile	=__DIR__.self::CACHE_FILE;
+			$fullMap	=array();
+			$iterator	=new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__));
+			$regex		=new RegexIterator($iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
+			foreach ($regex as $iteration)
+			{
+				require_once($iteration[0]);
+				$fileInfo	=pathinfo($iteration[0]);
+				$namespace	=str_replace(APP_HOME,'application\\',$fileInfo['dirname']);
+				$namespace	=str_replace('/','\\',$namespace);
+				$className	=$namespace.'\\'.$fileInfo['filename'];
+				$reflection	=new ReflectionClass($className);
+				$properties	=$reflection->getDefaultProperties();
+				if (isset($properties['map']))
+				{
+					$mapPath=str_replace(array(APP_HOME,'nutsNBolts/controller'),'',$fileInfo['dirname']).'/';
+					foreach ($properties['map'] as $pathNode=>$method)
+					{
+						$nodes=explode('/',$mapPath.lcfirst($fileInfo['filename']).'/'.$pathNode);
+						if (!end($nodes))	array_pop($nodes);
+						if (!reset($nodes))	array_shift($nodes);
+						$fullMap[$mapPath.lcfirst($fileInfo['filename']).'/'.$pathNode]=
+						[
+							'nodes'	=>$nodes,
+							'class'	=>$className,
+							'method'=>$method
+						];
+					}
+				}
+			}
+			file_put_contents($cacheFile,json_encode($fullMap));
+			return $this;
 		}
 	}
 }
