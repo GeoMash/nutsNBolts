31f688cee19b223cc371a9963f7dccbb1e7cfd02
Timothy Chandler <tim@praxisbt.com>
2014-04-17 18:14:30 +0800

    Updated map.

--- controller/rest/content/Message.php
+++ controller/rest/content/Message.php
@@ -3,19 +3,13 @@ namespace application\nutsNBolts\controller\rest\content
 {
 	use application\nutsNBolts\base\RestController;
 
-	class Node extends RestController
+	class Message extends RestController
 	{
-		public function init()
-		{
-			$this->bindPaths
-			(
-				array
-				(
-					''		=>'getAll',
-					'{int}'	=>'getById'
-				)
-			);
-		}
+		private $map=array
+		(
+			''		=>'getAll',
+			'{int}'	=>'getById'
+		);
 		
 		public function getAll()
 		{
