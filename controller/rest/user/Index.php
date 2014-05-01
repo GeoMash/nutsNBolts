454d95240ec8e9cabc8c0ab207725af4ddc29d5e
gabriele <gabriele@geomash.com>
2014-05-01 14:33:06 +0800

    Changed to use rest plugin

--- controller/rest/user/Index.php
+++ controller/rest/user/Index.php
@@ -1,7 +1,7 @@
 <?php
 namespace application\nutsNBolts\controller\rest\user
 {
-	use application\nutsNBolts\base\RestController;
+	use application\plugin\rest\RestController;
 
 	class Index extends RestController
 	{
