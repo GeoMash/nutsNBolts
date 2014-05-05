1bd4de182782ec40246fa6d67782af3dbb2d4171
Timothy Chandler <tim@praxisbt.com>
2014-05-05 09:16:34 +0800

    Fixed user login for generic user controller.

--- controller/User.php
+++ controller/User.php
@@ -37,98 +37,56 @@ namespace application\nutsNBolts\controller
 					
 		public function login($request)
 		{
-			if($request['user_id'])
+			if (isset($request['email']))
 			{
-				$fieldValue=preg_replace('/[^0-9]/', '',$request['user_id']);
-				$fieldName='phone';
-				$params=array($fieldName=>$fieldValue);
-				$user=$this->plugin->UserAuth->authenticate($params,$request['password']);
-				if($user)
+				$query=['email'=>$request['email']];
+			}
+			else if (isset($request['phone']))
+			{
+				$query=['phone'=>$request['phone']];
+			}
+			else
+			{
+				$this->plugin->Notification->setError("Username and/or password error");
+				header('location:/');
+				exit();
+			}
+			$user=$this->plugin->UserAuth->authenticate($query,$request['password']);
+			if ($user)
+			{
+				$dateTime=new DateTime();
+				if (isset($request['email']))
 				{
-
-                    if(strlen($user['email']) > 3)
-                    {
-                        $dt = new DateTime();
-                        Nutshell::getInstance()->plugin->Session->email=$user['email'];
-                        Nutshell::getInstance()->plugin->Session->phone=$user['phone'];
-                        Nutshell::getInstance()->plugin->Session->userId=$user['id'];
-                        Nutshell::getInstance()->plugin->Session->authenticated=true;
-                        $this->plugin->Mvc->model->User->update(array('date_lastlogin'=> $dt->format('Y-m-d H:i:s')),array('phone'=>$user['phone']));
-                        $this->filterRedirect($user['roles'][0]['ref']);
-                    }
-                    else
-                    {
-                        // user needs to fill in form
-                        Nutshell::getInstance()->plugin->Session->phone=$user['phone'];
-                        Nutshell::getInstance()->plugin->Session->authenticated=false;
-                        Nutshell::getInstance()->plugin->Session->password=$user['password'];
-                        Nutshell::getInstance()->plugin->Session->salt=$user['salt'];
-                        header('location:/login/complete');
-                        $this->plugin->Notification->setError("You need to fill in the form below before you can enter the website");
-                        exit();
-
-                    }
-
+					$this->plugin->Mvc->model->User->update(array('date_lastlogin'=> $dateTime->format('Y-m-d H:i:s')),array('email'=>$user['email']));
 				}
-				else
+				else if (isset($request['phone']))
 				{
-
-                    $this->plugin->Notification->setError("Username and/or password error");
-                    header('location:/');
-                    exit();
+					$this->plugin->Mvc->model->User->update(array('date_lastlogin'=> $dateTime->format('Y-m-d H:i:s')),array('phone'=>$user['phone']));
 				}
+				$session=Nutshell::getInstance()->plugin->Session;
+				$session->email=$user['email'];
+				$session->phone=$user['phone'];
+				$session->userId=$user['id'];
+				$session->authenticated=true;
+				
+				header('location:/');
+				exit();
+			}
+			else
+			{
+				$this->plugin->Notification->setError("Username and/or password error");
+				header('location:/');
+				exit();
 			}
 		}
 		
 		public function logout()
 		{
 			unset($this->plugin->Session->authenticated);
-			unset($this->plugin->Session->userId);		
+			unset($this->plugin->Session->userId);
 			header('location:/');
-			exit();			
+			exit();
 		}
-
-		
-		private function filterRedirect($role)
-        {
-			if(isset($role))
-			{
-				switch (strtoupper($role))
-				{
-					case 'SUPER':
-							header('location:/admin');
-							exit();
-						break;
-
-					case 'STANDARD':
-							header('location:/dashboard');
-							exit();
-						break;
-						
-					case 'BARTENDER':
-							header('location:/bartender');
-							exit();
-						break;
-
-					case 'MANAGER':
-							header('location:/manager');
-							exit();
-						break;
-
-					case 'ADMIN':
-							header('location:/admin');
-							exit();
-						break;
-
-					default:
-                        die('here');
-							header('location:/login');
-							exit();
-						break;
-				}
-			}
-		}		
-			
 	}
 }
 ?>
