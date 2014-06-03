9c9fe8b347cb1e290bbfbbfd0313c38696ba460b
Mehrdad Dastgir <md@praxisbt.com>
2014-06-03 17:16:24 +0800

    added the new icon sets and filemanager widget

---
+++ widget/file/assets/Main.js
@@ -0,0 +1,44 @@
+$JSKK.Class.create
+(
+	{
+		$namespace:	'nutsnbolts.widget.image',
+		$name:		'Main'
+	}
+)
+(
+	{},
+	{
+		widget:	null,
+		init: function(id)
+		{
+			this.widget=$('[data-id="'+id+'"]')
+			$application.registerAction('widget.image.main.browseImage',this.browseImage.bind(this))
+		},
+		browseImage: function()
+		{
+			$application.fileManager.show
+			(
+				{
+					buttons:
+					[
+						{
+							label:		'Select Image',
+							icon:		'icon-ok',
+							handler:	this.onSelect.bind(this)
+						}
+					]
+				}
+			);
+		},
+		onSelect: function(fileManager)
+		{
+			fileManager.hide();
+			var selected=fileManager.getSelected();
+			this.widget.find('.imageSelector').addClass('selected');
+			this.widget.find('input').val(selected.src);
+			this.widget.find('.thumbs a')	.attr('title',selected.title)
+											.css('background-image',selected.thumb)
+											.html('');
+		}
+	}
+);
