5609bdf05afc3b528c26a3132f008a7a263317a3
Timothy Chandler <tim@praxisbt.com>
2014-06-06 22:41:48 +0800

    Fixed class alias.

--- model/PermissionRole.php
+++ model/PermissionRole.php
@@ -1,7 +1,7 @@
 <?php
 namespace application\nutsNBolts\model
 {
-	use application\nutsNBolts\model\base\PermissionRole as PermissionBase;
+	use application\nutsNBolts\model\base\PermissionRole as PermissionRoleBase;
 	
 	class PermissionRole extends PermissionRoleBase	
 	{
