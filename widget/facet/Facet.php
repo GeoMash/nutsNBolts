65c97b916f66fa05a5629efdea83238dfc110801
Mehrdad Dastgir <md@praxisbt.com>
2014-06-10 18:14:32 +0800

    facet class

---
+++ widget/facet/Facet.php
@@ -0,0 +1,14 @@
+<?php
+namespace application\nutsNBolts\widget\facet
+{
+	use application\nutsNBolts\widget\base\Widget as WidgetBase;
+
+	class Facet extends WidgetBase
+	{
+		public function init()
+		{
+			
+		}
+	}
+}
+?>
