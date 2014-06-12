2789111658415e305aa1860b8088c596f5dc47ec
Mehrdad Dastgir <md@praxisbt.com>
2014-06-12 21:54:17 +0800

    [WARNING NEEDS APPROVAL DON'T DEPLOY] modifed the getWithParts function to allow passing in multiple ids for the query

--- model/Node.php
+++ model/Node.php
@@ -178,6 +178,7 @@ SQL;
 		public function getWithParts($whereKeyVals=array(),$offset=null,$limit=null,$status=self::STATUS_PUBLISHED)
 		{
 			$where=array();
+			$or=array();
 			if (count($whereKeyVals))
 			{
 				if (is_numeric($whereKeyVals))
@@ -192,7 +193,18 @@ SQL;
 						{
 							case 'id':
 							{
-								$where[]='node.id="'.addslashes($value).'"';
+								if(is_array($value))
+								{
+									for($i=0,$j=count($value);$i<$j;$i++)
+									{
+										$or[]='node.id="'.addslashes($value[$i]).'"';
+									}
+								}
+								else
+								{
+									$where[]='node.id="'.addslashes($value).'"';
+								}
+								
 								break;
 							}
 							case 'content_type_id':
@@ -222,6 +234,14 @@ SQL_PART;
 			{
 				$where='';
 			}
+			if($or)
+			{
+				$or='WHERE '.implode(' OR ',$or);
+			}
+			else
+			{
+				$or='';
+			}
 			if($offset && $limit)
             {
                 $limitSql=<<<SQL_PART
@@ -245,12 +265,14 @@ SQL_PART;
 				LEFT JOIN node_part ON node.id=node_part.node_id
 				LEFT JOIN content_part ON node_part.content_part_id=content_part.id
 				{$where}
+				{$or}
 			)
 			AND node.status={$status}
 			ORDER BY node.id ASC
 			{$limitSql}
 			;
 SQL;
+			
 			if ($result=$this->plugin->Db->nutsnbolts->select($query))
 			{
 				$records=$this->plugin->Db->nutsnbolts->result('assoc');
