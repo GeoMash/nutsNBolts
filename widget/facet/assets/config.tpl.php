8d8898ab17ef06380d7628c004a0c6f7c425665c
Timothy Chandler <tim@praxisbt.com>
2014-06-12 10:54:52 +0800

    modified the facet widget

---
+++ widget/facet/assets/config.tpl.php
@@ -0,0 +1,34 @@
+<div class="control-group">
+	<h5>Options:</h5>
+	<br />
+	<table class="table table-hover">
+		<thead>
+			<th>Label</th>
+			<th>Value</th>
+			<th>Delete</th>
+		</thead>
+		<tbody>
+			<?php
+			if ($tpl->get('options')):
+				$options=$tpl->get('options');
+				for ($i=0,$j=count($options); $i<$j; $i++):
+			?>
+					<tr>
+						<td>
+							<input type="text" class="input-medium" value="<?php print $options[$i]->label; ?>" data-prompt-position="topLeft" class="validate[required]" name="widget[<?php $tpl->widgetIndex; ?>][config][options][<?php print $i; ?>][label]">
+						</td>
+						<td>
+							<input type="text" class="input-medium" value="<?php print $options[$i]->value; ?>" data-prompt-position="topLeft" class="validate[required]" name="widget[<?php $tpl->widgetIndex; ?>][config][options][<?php print $i; ?>][value]">
+						</td>
+						<td>
+							<button data-action="widget.select.config.removeOption" class="btn btn-danger btn-mini" type="button">&times;</button>
+						</td>
+					</tr>
+			<?php
+				 endfor;
+			 endif;
+			?>
+		</tbody>
+	</table>
+	<button data-action="widget.select.config.addOption" class="btn btn-green" type="button"><i class="icon-plus"></i> Add Option</button>
+</div>
