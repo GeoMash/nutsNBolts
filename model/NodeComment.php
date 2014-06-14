383712c3dad9a63905d04385e37309796247b105
Mehrdad Dastgir <md@praxisbt.com>
2014-06-14 17:07:54 +0800

    over ride the read method

--- model/NodeComment.php
+++ model/NodeComment.php
@@ -2,10 +2,19 @@
 namespace application\nutsNBolts\model
 {
 	use application\nutsNBolts\model\base\NodeComment as NodeCommentBase;
-	
+	use \DateTime;
+
 	class NodeComment extends NodeCommentBase	
 	{
-		
+		public function read($whereKeyVals = array(), $readColumns = array(), $additionalPartSQL='')
+		{
+			$result=parent::read($whereKeyVals, $readColumns, $additionalPartSQL);
+			for ($i=0,$j=count($result); $i<$j; $i++)
+			{
+				$result[$i]['date_created']=new DateTime($result[$i]['date_created']);
+			}
+			return $result;		
+		}
 	}
 }
 ?>
