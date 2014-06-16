db7b915168076854c72205c44ba711864d31e24c
Timothy Chandler <tim@praxisbt.com>
2014-06-16 14:29:11 +0800

    Implemented permission checks in the admin views.

--- view/admin/nav/content.php
+++ view/admin/nav/content.php
@@ -1,4 +1,10 @@
-<?php if ($tpl->challengeRole(array('SUPER','ADMIN','CONTENT_CREATOR','CONTENT_EDITOR','BLOGGER'))): ?>
+<?php
+if ($tpl->can('admin.content.contentType.read')
+|| $tpl->can('admin.content.navigation.read')
+|| $tpl->can('admin.content.form.read')
+|| $tpl->can('admin.content.widget.read')
+|| $tpl->can('admin.content.node.read')):
+?>
 <li class="dropdown <?php if ($tpl->get('nav_active_main')=='content')print 'active'; ?>">
 	<a class="dropdown-toggle" href="#" data-toggle="dropdown">
 		<i class="icon-edit icon-2x"></i>
@@ -8,30 +14,48 @@
 		</span>
 	</a>
 	<ul class="dropdown-menu">
-		<?php if ($tpl->challengeRole(array('SUPER','ADMIN'))): ?>
+		<?php if ($tpl->can('admin.content.contentType.read')): ?>
 		<li class="<?php if ($tpl->get('nav_active_sub')=='types')print 'active'; ?>">
 			<a href="/admin/configurecontent/types">
 				<i class="icon-th"></i> Content Types
 			</a>
 		</li>
+		<?php
+			endif;
+			if ($tpl->can('admin.content.nav.read')):
+		?>
 		<li class="<?php if ($tpl->get('nav_active_sub')=='nav')print 'active'; ?>">
 			<a href="/admin/configurecontent/nav">
 				<i class="icon-beaker"></i> Navigation
 			</a>
 		</li>
+		<?php
+			endif;
+			if ($tpl->can('admin.content.form.read')):
+		?>
 		<li class="<?php if ($tpl->get('nav_active_sub')=='forms')print 'active'; ?>">
 			<a href="/admin/configurecontent/forms">
 				<i class="icon-list-alt"></i> Forms
 			</a>
 		</li>
+		<?php
+			endif;
+			if ($tpl->can('admin.content.widget.read')):
+		?>
 		<li class="<?php if ($tpl->get('nav_active_sub')=='widgets')print 'active'; ?>" style="display:none;">
 			<a href="/admin/configurecontent/widgets">
 				<i class="icon-beaker"></i> Widgets
 			</a>
 		</li>
-		<?php endif; ?>
+		<?php
+			endif;
+			if ($tpl->can('admin.content.node.read')):
+		?>
 		<li class="divider"></li>
-		<?php $tpl->navContentTypes(); ?>
+		<?php
+			$tpl->navContentTypes();
+			endif;
+		?>
 	</ul>
 </li>
 <?php endif; ?>
