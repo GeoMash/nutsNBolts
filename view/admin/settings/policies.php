2502b7c32875ca37e3a1a8b9bf72bc3e92b40ced
Timothy Chandler <tim@praxisbt.com>
2014-06-21 13:49:15 +0800

    Policy values save and restore.

--- view/admin/settings/policies.php
+++ view/admin/settings/policies.php
@@ -47,45 +47,45 @@ $record=$tpl->get('record');
 										</tr>
 										<tr>
 											<td>
-												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked(''); ?> name="[enabled]" value="1">
+												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked('password_special_characters'); ?> name="password_special_characters[enabled]" value="1">
 											</td>
 											<td>Must Contain Special Characters</td>
 											<td></td>
 										</tr>
 										<tr>
 											<td>
-												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked(''); ?> name="[enabled]" value="1">
+												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked('password_numeric_digits'); ?> name="password_numeric_digits[enabled]" value="1">
 											</td>
 											<td>Must Contain Numeric Digits</td>
 											<td></td>
 										</tr>
 										<tr>
 											<td>
-												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked(''); ?> name="[enabled]" value="1">
+												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked('password_upper_lower_characters'); ?> name="password_upper_lower_characters[enabled]" value="1">
 											</td>
 											<td>Must Contain Upper and Lower Case Characters</td>
 											<td></td>
 										</tr>
 										<tr>
 											<td>
-												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked(''); ?> name="[enabled]" value="1">
+												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked('password_no_user_details'); ?> name="password_no_user_details[enabled]" value="1">
 											</td>
 											<td>Must Not Contain Any User Details</td>
 											<td></td>
 										</tr>
 										<tr>
 											<td>
-												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked(''); ?> name="[enabled]" value="1">
+												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked('password_expiry'); ?> name="password_expiry[enabled]" value="1">
 											</td>
 											<td>Password Expiry</td>
-											<td><input class="input-small" type="number" name="[value]" value="<?php print $record[''] ?>"><span class="help-inline">Days</span></td>
+											<td><input class="input-small" type="number" name="password_expiry[value]" value="<?php print $record['password_expiry'] ?>"><span class="help-inline">Days</span></td>
 										</tr>
 										<tr>
 											<td>
-												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked(''); ?> name="[enabled]" value="1">
+												<input class="iButton-icons-tab" type="checkbox" <?php $tpl->isChecked('password_past_passwords'); ?> name="password_past_passwords[enabled]" value="1">
 											</td>
 											<td>Prevent Past Passwords</td>
-											<td><input class="input-small" type="number" name="[value]" value="<?php print $record[''] ?>"><span class="help-inline">Past Passwords</span></td>
+											<td><input class="input-small" type="number" name="password_past_passwords[value]" value="<?php print $record['password_past_passwords'] ?>"><span class="help-inline">Past Passwords</span></td>
 										</tr>
 									</tbody>
 								</table>
