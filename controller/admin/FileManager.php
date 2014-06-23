16cbfd0c968d993b1218652187687eae9274f7f4
Mehrdad Dastgir <md@praxisbt.com>
2014-06-23 12:28:14 +0800

    caters for profile picture upload

--- controller/admin/FileManager.php
+++ controller/admin/FileManager.php
@@ -17,6 +17,18 @@ namespace application\nutsNBolts\controller\admin
 		
 		public function upload($collectionID)
 		{
+			if(!$collectionID)
+			{
+				$collections=$this->model->CollectionUser->read
+				(
+					[
+						'user_id'		=>$this->plugin->Session->userId
+					]	
+				);
+
+				$collectionID=$collections[0]['collection_id'];
+			}
+			
 			try
 			{
 				$this->plugin->Auth->can('admin.collection.create');
