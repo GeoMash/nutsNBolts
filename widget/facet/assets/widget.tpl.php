ef86eada14deded2008568b10071bec57ee33d57
Mehrdad Dastgir <md@praxisbt.com>
2014-06-25 19:48:49 +0800

    fixed the facet widget

--- widget/facet/assets/widget.tpl.php
+++ widget/facet/assets/widget.tpl.php
@@ -1,10 +1,21 @@
-<select data-id="<?php $tpl->dataId; ?>" name="<?php $tpl->name; ?>">
+<select multiple data-id="<?php $tpl->dataId; ?>" name="<?php $tpl->name; ?>[]">
 	<?php
-	$options=$tpl->get('options');
-	for ($i=0,$j=count($options); $i<$j; $i++)
-	{
-		$selected=($options[$i]->value!=$tpl->get('value'))?'':'selected';
-		print '<option value="'.$options[$i]->value.'" '.$selected.'>'.$options[$i]->label.'</option>';
-	}
+		$value=$tpl->get('value');
+		$value=json_decode(str_replace('application/json: ','',$value),true);	
+		$options=$tpl->get('options');
+		for ($i=0,$j=count($options); $i<$j; $i++)
+		{
+			$selected=(in_array($options[$i]->value,$value?'selected':''));
+			if(in_array($options[$i]->value,$value))
+			{
+				$selected=' selected ';
+			}
+			else
+			{
+				$selected='';
+			}
+			
+			print '<option value="'.$options[$i]->value.'" '.$selected.'>'.$options[$i]->label.'</option>';
+		}
 	?>
 </select>
