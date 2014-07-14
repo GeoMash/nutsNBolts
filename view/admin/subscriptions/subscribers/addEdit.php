d1abae5ef6aa77e21540ea7c3006ea7f985dd373
Hasan Baidoun <hasan.baidoun@geomash.com>
2014-07-14 11:14:08 +0800

    Reformatting

--- view/admin/subscriptions/subscribers/addEdit.php
+++ view/admin/subscriptions/subscribers/addEdit.php
@@ -4,100 +4,115 @@
 			<div class="box">
 				<form class="form-horizontal fill-up validatable" method="post">
 					<input type="hidden" name="id" value="<?php $tpl->id; ?>">
+
 					<div class="box-header">
 						<span class="title">User Subscription Details</span>
 					</div>
 					<div class="box-content">
 						<?php
-						$extras=$tpl->get('aboveForm');
+						$extras = $tpl->get('aboveForm');
 						foreach ($extras as $extra)
 						{
 							print $extra;
 						}
 						?>
 						<div class="padded">
-							
+
 							<div class="control-group">
 								<label class="control-label">Package Name</label>
+
 								<div class="controls">
 									<select class="input-small" name="subscription_id">
 										<?php
 										$subscriptions = $tpl->getSubscriptions();
-										for($i = 0; $i < count($subscriptions); $i++):
-										?>
-										<option <?php if($tpl->get('subscription_id') == $subscriptions[$i]['id']) print "selected=\"true\""; ?> value="<?php print $subscriptions[$i]['id'] ?>"><?php print $subscriptions[$i]['name'] ?></option>
+										for ($i = 0; $i < count($subscriptions); $i++):
+											?>
+											<option <?php if ($tpl->get('subscription_id') == $subscriptions[$i]['id']) print "selected=\"true\""; ?>
+												value="<?php print $subscriptions[$i]['id'] ?>"><?php print $subscriptions[$i]['name'] ?></option>
 										<?php endfor; ?>
 									</select>
 								</div>
 							</div>
-							
+
 							<div class="control-group">
 								<label class="control-label">Subscriber Name</label>
+
 								<div class="controls">
 									<input data-role="selectUser" name="user_id" value=<?php $tpl->user_id; ?>>
 								</div>
 							</div>
-														
+
 							<div class="control-group">
 								<label class="control-label">Creation Date</label>
+
 								<div class="controls">
-									<input type="text" name="timestamp" class="validate[required] datepicker-subscription-timestamp" value="<?php $tpl->timestamp; ?>">
+									<input type="text" name="timestamp"
+										   class="validate[required] datepicker-subscription-timestamp"
+										   value="<?php $tpl->timestamp; ?>">
 								</div>
 							</div>
-							
+
 							<div class="control-group">
 								<label class="control-label">Expiry Date</label>
+
 								<div class="controls">
-									<input type="text" name="expiry_timestamp" class="datepicker-subscription-timestamp" data-prompt-position="topLeft" value="<?php $tpl->expiry_timestamp; ?>">
+									<input type="text" name="expiry_timestamp" class="datepicker-subscription-timestamp"
+										   data-prompt-position="topLeft" value="<?php $tpl->expiry_timestamp; ?>">
 								</div>
 							</div>
-							
+
 							<div class="control-group">
 								<label class="control-label">Status</label>
+
 								<div class="controls">
 									<select class="input-small" name="status">
 										<?php
 										$statuses = $tpl->getStatues();
-										for($i = 0; $i < count($statuses); $i++):
-										?>
-										<option <?php if($tpl->get('status') == $statuses[$i]) print "selected=\"true\""; ?> value="<?php print $statuses[$i] ?>"><?php print $tpl->formatStatus($statuses[$i]) ?></option>
+										for ($i = 0; $i < count($statuses); $i++):
+											?>
+											<option <?php if ($tpl->get('status') == $statuses[$i]) print "selected=\"true\""; ?>
+												value="<?php print $statuses[$i] ?>"><?php print $tpl->formatStatus($statuses[$i]) ?></option>
 										<?php endfor; ?>
 									</select>
 								</div>
 							</div>
-							
-							<?php if($tpl->get('extraOptions')['isAdding']): ?>
-							<div class="control-group">
-								<label class="control-label">Is New</label>
-								<div class="controls">
-									<input type="checkbox">
+
+							<?php if ($tpl->get('extraOptions')['isAdding']): ?>
+								<div class="control-group">
+									<label class="control-label">Is New</label>
+
+									<div class="controls">
+										<input type="checkbox" name="is_new">
+									</div>
 								</div>
-							</div>
-							
-							<div class="control-group">
-								<label class="control-label">Credit Card No.</label>
-								<div class="controls">
-									<input data-role="selectUser" name="user_id" value=<?php $tpl->user_id; ?>>
+
+								<div class="control-group">
+									<label class="control-label">Credit Card No.</label>
+
+									<div class="controls">
+										<input data-role="selectUser" name="card_no" value=<?php $tpl->user_id; ?>>
+									</div>
 								</div>
-							</div>
-							
-							<div class="control-group">
-								<label class="control-label">Credit Card Code</label>
-								<div class="controls">
-									<input data-role="selectUser" name="user_id" value=<?php $tpl->user_id; ?>>
+
+								<div class="control-group">
+									<label class="control-label">Credit Card Code</label>
+
+									<div class="controls">
+										<input data-role="selectUser" name="ccv" value=<?php $tpl->user_id; ?>>
+									</div>
 								</div>
-							</div>
-							
-							<div class="control-group">
-								<label class="control-label">Credit Card Expiry Date</label>
-								<div class="controls">
-									<input data-role="selectUser" name="user_id" value=<?php $tpl->user_id; ?>>
+
+								<div class="control-group">
+									<label class="control-label">Credit Card Expiry Date</label>
+
+									<div class="controls">
+										<input data-role="selectUser" name="card_exp" value=<?php $tpl->user_id; ?>>
+									</div>
 								</div>
-							</div>
 							<?php endif; ?>
 						</div>
 						<?php
-						$extras=$tpl->get('belowForm');
+						$extras = $tpl->get('belowForm');
 						foreach ($extras as $extra)
 						{
 							print $extra;
