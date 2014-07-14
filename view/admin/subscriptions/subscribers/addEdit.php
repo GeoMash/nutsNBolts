5ed4a376a4daee79fffd41a019d909def2b9598f
Hasan Baidoun <hasan.baidoun@geomash.com>
2014-07-14 14:12:58 +0800

    Fix: using select element for CC Expiry Date

--- view/admin/subscriptions/subscribers/addEdit.php
+++ view/admin/subscriptions/subscribers/addEdit.php
@@ -90,7 +90,7 @@
 									<label class="control-label">Credit Card No.</label>
 
 									<div class="controls">
-										<input data-role="selectUser" name="card_no" value=<?php $tpl->user_id; ?>>
+										<input type="text" name="cc[number]" value=<?php $tpl->user_id; ?>>
 									</div>
 								</div>
 
@@ -98,15 +98,49 @@
 									<label class="control-label">Credit Card Code</label>
 
 									<div class="controls">
-										<input data-role="selectUser" name="ccv" value=<?php $tpl->user_id; ?>>
+										<input type="text" name="cc[ccv]" value=<?php $tpl->user_id; ?>>
 									</div>
 								</div>
 
 								<div class="control-group">
-									<label class="control-label">Credit Card Expiry Date</label>
+									<label class="control-label">Credit Card Expiry Month</label>
 
 									<div class="controls">
-										<input data-role="selectUser" name="card_exp" value=<?php $tpl->user_id; ?>>
+										<select name="cc[expiry-month]" class="form-control">
+											<option value="">Expiry Month</option>
+											<option value="1">1</option>
+											<option value="2">2</option>
+											<option value="3">3</option>
+											<option value="4">4</option>
+											<option value="5">5</option>
+											<option value="6">6</option>
+											<option value="7">7</option>
+											<option value="8">8</option>
+											<option value="9">9</option>
+											<option value="10">10</option>
+											<option value="11">11</option>
+											<option value="12">12</option>
+										</select>
+									</div>
+								</div>
+								
+								<div class="control-group">
+									<label class="control-label">Credit Card Expiry Year</label>
+
+									<div class="controls">
+										<select name="cc[expiry-year]" class="form-control">
+											<option value="">Expiry Year</option>
+											<option value="14">14</option>
+											<option value="15">15</option>
+											<option value="16">16</option>
+											<option value="17">17</option>
+											<option value="18">18</option>
+											<option value="19">19</option>
+											<option value="20">20</option>
+											<option value="21">21</option>
+											<option value="22">22</option>
+											<option value="23">23</option>
+										</select>
 									</div>
 								</div>
 							<?php endif; ?>
