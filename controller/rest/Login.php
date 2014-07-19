abc3c2cc9a2020713cbb8f70190cc64ddef5e482
Timothy Chandler <tim@praxisbt.com>
2014-07-19 21:51:24 +0800

    Cleaned up the login REST controller.

--- controller/rest/Login.php
+++ controller/rest/Login.php
@@ -10,53 +10,8 @@ namespace application\nutsNBolts\controller\rest
 	{
 		private $map=array
 		(
-			'{int}'						=>'checkById',
 			'loginByEmail'				=>'loginByEmail'
 		);
-		
-		/*
-		 * sample request: $.getJSON('/rest/login/1.json');
-		 */
-		public function checkById()
-		{
-			$userId=$this->getFullRequestPart(2);
-			if(isset($userId) && is_numeric($userId))
-			{
-
-				if(Nutshell::getInstance()->plugin->Session->authenticated)
-				{
-					// logged in
-					$this->setResponseCode(200);
-					$this->respond
-					(
-						true,
-						'OK',
-						true
-					);								
-				}
-				else
-				{
-					// logged out
-					$this->setResponseCode(200);
-					$this->respond
-					(
-						true,
-						'OK',
-						false
-					);								
-				}
-
-			}
-			else
-			{
-				$this->setResponseCode(404);
-				$this->respond
-				(
-					false,
-					'Expecting user id to be an integer.'
-				);
-			}
-		}
 		
 		/*
 		 * sample request: $.getJSON('/rest/login/loginByEmail/xxx/***.json');
@@ -69,31 +24,15 @@ namespace application\nutsNBolts\controller\rest
 				[
 					'email'=>$this->request->get('email')
 				],
-					$this->request->get('password')
+				$this->request->get('password')
 			);
 			if ($user)
 			{
-				$dateTime=new DateTime();
-				if (isset($this->request->get['email']))
-				{
-					$this->plugin->Mvc->model->User->update(array('date_lastlogin'=> $dateTime->format('Y-m-d H:i:s')),array('email'=>$user['email']));
-				}
-				else if (isset($this->request->get['phone']))
-				{
-					$this->plugin->Mvc->model->User->update(array('date_lastlogin'=> $dateTime->format('Y-m-d H:i:s')),array('phone'=>$user['phone']));
-				}
-				$session=Nutshell::getInstance()->plugin->Session;
-				$session->email=$user['email'];
-				$session->phone=$user['phone'];
-				$session->userId=$user['id'];
-				$session->authenticated=true;
-				
 				$data=true;
 				if (!empty($this->plugin->Session->returnURL))
 				{
 					$data=$this->plugin->Session->returnURL;
 				}
-				
 				$this->setResponseCode(200);
 				$this->respond
 				(
