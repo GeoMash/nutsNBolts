de9d8f830b97a7562171d7f28bd37839980870a1
Timothy Chandler <tim@praxisbt.com>
2014-07-30 02:48:09 +0800

    All your search are belong to me.

--- controller/Sandbox.php
+++ controller/Sandbox.php
@@ -1,66 +1,26 @@
 <?php
-namespace application\nutsNBolts\controller {
-	use nutshell\core\exception\ApplicationException;
-	use nutshell\Nutshell;
-	use nutshell\core\exception\NutshellException;
+namespace application\nutsNBolts\controller
+{
 	use nutshell\plugin\mvc\Controller;
-	use application\nutsNBolts\Encoding;
 
 	class Sandbox extends Controller
 	{
 		public function index()
 		{
-			try {
-				//$date = new \DateTime('now');
-				//echo $date->format('Y-m-d');
-				//$authorizeNet = $this->plugin->Payment('AuthorizeNet');
-				//$response = $authorizeNet->deleteRecurringSubscription('2114398');
-				//$response = $authorizeNet->createRecurringSubscription('200.00','4012888818888','124','10/16');
-				//$response = $authorizeNet->chargeCard('4012888818888',null,'10/16','20.00','hasan.baidoun@geomash.com');
-				//var_dump($response);
-
-				//var_dump($this->model->Subscription->read());
-
-				$subscription = $this->plugin->Subscription();
-				$subscription->subscribe('100', '2', '370000000000002', '152', '10', '2016');
-			} catch (ApplicationException $exp) {
-				die($exp->getMessage());
-			}
+			$search=$this->plugin->Search('Announcements');
+			$search->setContentType(2);
+			$search->clearCache();
+			$search->addFilter
+			(
+				[
+//					'id'=>'>10'
+					'body'=>'LIKE "%Par Value%"'
+				]
+			);
+			$results=$search->execute();
+			
+			var_dump($results);
 		}
-
-		public function cancelArb($arbId)
-		{
-			$payment = $this->plugin->Payment("AuthorizeNet");
-			$payment->deleteRecurringSubscription($arbId);
-		}
-
-		public function voidTransaction($transactionId)
-		{
-			$payment = $this->plugin->Payment("AuthorizeNet");
-			$payment->voidTransaction($transactionId);
-		}
-//		public function index()
-//		{
-//
-//            $string = "ABC [Test1] and your pin ins [123456]";
-//            echo preg_replace('/\[.*?\]/', '', $string);
-//
-//			$mobileNumber	='0172359029';
-//			$message		='WTP SMS Test 000001';
-//			$SMS			=$this->plugin->Sms('M3Tech');
-//			$SMS->setMobileNumber($mobileNumber)
-//				->setMessage($message)
-//				->send();
-//			$this->model->Sms->insertAssoc
-//			(
-//				array
-//				(
-//					'bar_id'=>175,
-//					'user_id'=>1,
-//					'message'=>$message
-//				)
-//			);
-//		}
 	}
 }
 ?>
---
+++ plugin/search/Search.php
@@ -0,0 +1,218 @@
+<?php
+namespace application\nutsNBolts\plugin\search
+{
+	use application\nutsNBolts\plugin\search\exception\SearchException;
+	use nutshell\behaviour\Native;
+	use nutshell\behaviour\Factory;
+	use application\nutsNBolts\base\Plugin;
+	use nutshell\Nutshell;
+	use DateTime;
+
+	class Search extends Plugin implements Factory, Native
+	{
+		const TABLE_NAME_PREFIX='search_';
+
+		private $db			=null;
+		private $contentType=null;
+		private $name		=null;
+		private $filter		=[];
+		
+		public function init($name)
+		{
+			if ($connection=Nutshell::getInstance()->config->plugin->Mvc->connection)
+			{
+				//Make a shortcut reference to the
+				$this->db=$this->plugin->Db->{$connection};
+			}
+			$this->name=$name;
+		}
+		
+		public function getDb()
+		{
+			return $this->db;
+		}
+		
+		public function setContentType($contentTypeId)
+		{
+			$this->contentType=$contentTypeId;
+			return $this;
+		}
+		
+		public function execute()
+		{
+			$userId		=$this->plugin->Auth->getUserId();
+			$tableName	=self::TABLE_NAME_PREFIX.$this->name;
+			
+			if (!$this->cacheExists($tableName))
+			{
+				$nodes=$this->model->Node->getWithParts(['content_type_id'=>$this->contentType]);
+				//TODO: Handle zero results.
+				if (isset($nodes[0]))
+				{
+					$this->buildTable($nodes[0]);
+					$keys				=array_keys($nodes[0]);
+					$columnCount		=count($keys);
+					$columns			='`'.implode('`,`',$keys).'`';
+					$valuePlaceholders	=rtrim(str_repeat('?,',$columnCount),',');
+					$query				=<<<SQL
+					INSERT INTO `{$tableName}`
+					({$columns})
+					VALUES
+					({$valuePlaceholders});
+SQL;
+					for ($i=0,$j=count($nodes); $i<$j; $i++)
+					{
+						$values=array_values($nodes[$i]);
+						$this->sanitizeValues($values);
+						$this->getDb()->query($query,$values);
+					}
+				}
+			}
+			
+			//Now build the actual search query.
+			$values=[];
+			if (count($this->filter))
+			{
+				$where=[];
+				foreach ($this->filter as $column=>$value)
+				{
+					if (strtolower(substr($value,0,4))=='like')
+					{
+						$where[]='`'.$column.'` '.$value;
+						continue;
+					}
+					else if (in_array(substr($value,0,1),['=','<','>']))
+					{
+						$operand	=substr($value,0,1);
+						$values[]	=substr($value,1);
+					}
+					else if (in_array(substr($value,0,2),['!=','<>']))
+					{
+						$operand	=substr($value,0,2);
+						$values[]	=substr($value,2);
+					}
+					else
+					{
+						$operand	='=';
+						$values[]	=$value;
+					}
+					$where[]='`'.$column.'`'.$operand.'?';
+				}
+				$where='WHERE '.implode(' AND ',$where);
+			}
+			else
+			{
+				$where='';
+			}
+			
+			$query=<<<SQL
+SELECT *
+FROM `{$tableName}`
+{$where}
+SQL;
+			$this->getDb()->query($query,$values);
+			return $this->getDb()->result('assoc');
+		}
+		
+		public function clearCache()
+		{
+			$name=self::TABLE_NAME_PREFIX.$this->name;
+			$this->getDb()->query('DROP TABLE IF EXISTS `'.$name.'`;');
+			return $this;
+		}
+		
+		public function addFilter(Array $keyVals)
+		{
+			foreach ($keyVals as $column=>$value)
+			{
+				$this->filter[$column]=$value;
+			}
+			return $this;
+		}
+		
+		public function removeFilter($column)
+		{
+			unset($this->filter[$column]);
+			return $this;
+		}
+		
+		public function clearFilter()
+		{
+			unset($this->filter);
+			$this->filter=[];
+			return $this;
+		}
+		
+		private function cacheExists($name)
+		{
+			$this->getDb()->query('SHOW TABLES LIKE ?',$name);
+			$result=$this->getDb()->result('indexed');
+			if (isset($result[0][0]))
+			{
+				return ($result[0][0]==$name);
+			}
+			return false;
+		}
+		
+		private function buildTable(Array &$node)
+		{
+			$keys		=array_keys($node);
+			$columns	=[];
+			
+			for ($i=0,$j=count($keys); $i<$j; $i++)
+			{
+				if ($keys[$i]=='id')
+				{
+					$columns[]='`id` INT(10) NOT NULL';
+				}
+				else if (substr($keys[$i],-3,3)=='_id')
+				{
+					$columns[]='`'.$keys[$i].'` INT(10) NULL';
+				}
+				else if ($keys[$i]=='order')
+				{
+					$columns[]='`order` INT(5) NULL DEFAULT "0"';
+				}
+				else if ($keys[$i]=='status')
+				{
+					$columns[]='`status` TINYINT(1) NULL DEFAULT "0"';
+				}
+				else if ($keys[$i]=='title')
+				{
+					$columns[]='`title` VARCHAR(100) NULL';
+				}
+				else if (substr($keys[$i],0,4)=='date')
+				{
+					$columns[]='`'.$keys[$i].'` TIMESTAMP NULL DEFAULT "0000-00-00 00:00:00"';
+				}
+				else
+				{
+					$columns[]='`'.$keys[$i].'` TEXT';
+				}
+			}
+			
+			$columns=implode(",\n",$columns);
+			$name	=self::TABLE_NAME_PREFIX.$this->name;
+			$query=<<<SQL
+CREATE TABLE `{$name}`
+(
+	{$columns},
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SQL;
+			$this->getDb()->query($query);
+			return $this;
+		}
+		
+		private function sanitizeValues(Array &$values)
+		{
+			for ($i=0,$j=count($values); $i<$j; $i++)
+			{
+				if ($values[$i] instanceof DateTime)
+				{
+					$values[$i]=$values[$i]->format('Y-m-d H:i:s');
+				}
+			}
+		}
+	}
+}
