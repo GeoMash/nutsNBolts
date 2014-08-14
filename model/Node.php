de8a1a5e733b460f05b931b36e4241760c274dfa
Timothy Chandler <tim@praxisbt.com>
2014-08-14 22:30:29 +0800

    Added label back.

--- model/Node.php
+++ model/Node.php
@@ -371,7 +371,6 @@ SQL;
 				$nodes=array();
 				for ($i=0,$j=count($records); $i<$j; $i++)
 				{
-					unset($records[$i]['label']);
 					if (!isset($nodes[$records[$i]['id']]))
 					{
 						$nodes[$records[$i]['id']]=ArrayHelper::withoutKey
